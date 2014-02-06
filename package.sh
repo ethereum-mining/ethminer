8d5935e8db1b8a8bb4391d3fa877537285a86fc8
Gav Wood <i@gavwood.com>
2014-02-06 14:51:42 +0000

    Debian packaging tweaks.

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-rm -f ../cpp-ethereum_*
+rm -f ../cpp-ethereum_*_source.changes
 debuild -S -sa
 cd ..
 dput -f ppa:r-launchpad-gavofyork-fastmail-fm/ethereum ethereum_*_source.changes
