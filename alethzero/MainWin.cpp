451fdb395a7e19723dd952972375d09a58a2f441
Tim Hughes <tim@twistedfury.com>
2014-03-02 18:24:49 +0000

    Drop https requirement for VS2013 for now.

--- alethzero/MainWin.cpp
+++ alethzero/MainWin.cpp
@@ -122,7 +122,11 @@ Main::Main(QWidget *parent) :
 	{
 		m_servers = QString::fromUtf8(_r->readAll()).split("\n", QString::SkipEmptyParts);
 	});
+#ifdef _MSC_VER
+	QNetworkRequest r(QUrl("http://www.ethereum.org/servers.poc3.txt"));
+#else
 	QNetworkRequest r(QUrl("https://www.ethereum.org/servers.poc3.txt"));
+#endif
 	r.setHeader(QNetworkRequest::UserAgentHeader, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1712.0 Safari/537.36");
 	m_webCtrl.get(r);
 	srand(time(0));
