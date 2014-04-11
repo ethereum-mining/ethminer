ca7316c0ff47fc6617bb04cae94fd2eaf18d2550
Gav Wood <i@gavwood.com>
2014-04-11 01:01:19 -0400

    NameReg.

--- alethzero/MainWin.cpp
+++ alethzero/MainWin.cpp
@@ -269,7 +269,7 @@ string htmlDump(bytes const& _b, unsigned _w = 8)
 	return ret.str();
 }
 
-Address c_config = Address("ccdeac59d35627b7de09332e819d5159e7bb7250");
+Address c_config = Address("5620133321fcac7f15a5c570016f6cb6dc263f9d");
 
 using namespace boost::process;
 
