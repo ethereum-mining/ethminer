ab76b78dd6500f6da37ade6e1fb51655ddcbd4ae
Gav Wood <i@gavwood.com>
2014-04-28 12:09:29 +0100

    Documentation.

---
+++ libethereum/ExtVMFace.cpp
@@ -0,0 +1,38 @@
+/*
+	This file is part of cpp-ethereum.
+
+	cpp-ethereum is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	cpp-ethereum is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** @file ExtVMFace.cpp
+ * @author Gav Wood <i@gavwood.com>
+ * @date 2014
+ */
+
+#include "ExtVMFace.h"
+
+using namespace std;
+using namespace eth;
+
+ExtVMFace::ExtVMFace(Address _myAddress, Address _caller, Address _origin, u256 _value, u256 _gasPrice, bytesConstRef _data, bytesConstRef _code, BlockInfo const& _previousBlock, BlockInfo const& _currentBlock):
+	myAddress(_myAddress),
+	caller(_caller),
+	origin(_origin),
+	value(_value),
+	gasPrice(_gasPrice),
+	data(_data),
+	code(_code),
+	previousBlock(_previousBlock),
+	currentBlock(_currentBlock)
+{}
+
