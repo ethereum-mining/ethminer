617f53b58a222c5ccfebfdf5ea03bb121ba3231c
Gav Wood <i@gavwood.com>
2014-05-30 00:45:34 +0200

    Protocol bump for previous error.

--- libethcore/CommonEth.cpp
+++ libethcore/CommonEth.cpp
@@ -28,7 +28,7 @@ using namespace eth;
 
 //#define ETH_ADDRESS_DEBUG 1
 
-const unsigned eth::c_protocolVersion = 18;
+const unsigned eth::c_protocolVersion = 19;
 
 static const vector<pair<u256, string>> g_units =
 {
