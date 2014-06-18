5f622e2ac1b224bf6093536cd3ec97ca397777fa
Tim Hughes <tim@twistedfury.com>
2014-06-18 12:24:56 +0100

    Fixed MSVC compile errors (but missing functionality in LLL parser)

--- neth/main.cpp
+++ neth/main.cpp
@@ -1022,7 +1022,7 @@ vector<string> form_dialog(vector<string> _sv, vector<string> _lv, vector<string
 	int _lfields = _lv.size();
 	int _bfields = _bv.size();
 	int maxfields = _sfields + _lfields + _bfields;
-	FIELD *field[maxfields];
+	vector<FIELD*> field(maxfields);
 
 	int ch;
 	int starty = 6;
@@ -1061,7 +1061,7 @@ vector<string> form_dialog(vector<string> _sv, vector<string> _lv, vector<string
 	field[maxfields] = NULL;
 
 	// Create the form and post it
-	FORM *form = new_form(field);
+	FORM *form = new_form(field.data());
 
 	// Calculate the area required for the form
 	scale_form(form, &_rows, &_cols);
