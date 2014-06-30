d861a48b976f37cdba481d0c6237bea41b102d78
Gav Wood <i@gavwood.com>
2014-06-30 11:49:52 +0200

    Docs & consolidation of headers.

---
+++ libethcore/All.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "BlockInfo.h"
+#include "CommonEth.h"
+#include "Dagger.h"
+#include "FileSystem.h"
+#include "MemoryDB.h"
+#include "OverlayDB.h"
+#include "SHA3.h"
+#include "TrieCommon.h"
+#include "TrieDB.h"
+#include "UPnP.h"
---
+++ libethereum/All.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "AddressState.h"
+#include "BlockChain.h"
+#include "Client.h"
+#include "Defaults.h"
+#include "Executive.h"
+#include "ExtVM.h"
+#include "PeerNetwork.h"
+#include "PeerServer.h"
+#include "PeerSession.h"
+#include "State.h"
+#include "Transaction.h"
+#include "TransactionQueue.h"
