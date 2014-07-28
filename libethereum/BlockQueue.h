2d37f0151d0837655f7a668fced7e059c8d4ab36
Gav Wood <i@gavwood.com>
2014-07-29 01:25:55 +0200

    Fixed block buffer issue.

--- libethereum/BlockQueue.h
+++ libethereum/BlockQueue.h
@@ -43,7 +43,11 @@ public:
 	bool import(bytesConstRef _tx, BlockChain const& _bc);
 
 	/// Grabs the blocks that are ready, giving them in the correct order for insertion into the chain.
-	void drain(std::vector<bytes>& o_out) { WriteGuard l(m_lock); swap(o_out, m_ready); m_readySet.clear(); }
+	/// Don't forget to call doneDrain() once you're done importing.
+	void drain(std::vector<bytes>& o_out) { WriteGuard l(m_lock); if (m_drainingSet.empty()) { swap(o_out, m_ready); swap(m_drainingSet, m_readySet); } }
+
+	/// Must be called after a drain() call. Notes that the drained blocks have been imported into the blockchain, so we can forget about them.
+	void doneDrain() { WriteGuard l(m_lock); m_drainingSet.clear(); }
 
 	/// Notify the queue that the chain has changed and a new block has attained 'ready' status (i.e. is in the chain).
 	void noteReady(h256 _b) { WriteGuard l(m_lock); noteReadyWithoutWriteGuard(_b); }
@@ -56,6 +60,7 @@ private:
 
 	mutable boost::shared_mutex m_lock;						///< General lock.
 	std::set<h256> m_readySet;								///< All blocks ready for chain-import.
+	std::set<h256> m_drainingSet;							///< All blocks being imported.
 	std::vector<bytes> m_ready;								///< List of blocks, in correct order, ready for chain-import.
 	std::set<h256> m_futureSet;								///< Set of all blocks whose parents are not ready/in-chain.
 	std::multimap<h256, std::pair<h256, bytes>> m_future;	///< For transactions that have an unknown parent; we map their parent hash to the block stuff, and insert once the block appears.
