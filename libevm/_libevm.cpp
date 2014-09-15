00d461a74ad0b96d72afb674d12c25471e98e14c
Tim Hughes <tim@twistedfury.com>
2014-09-15 19:19:22 +0100

    Disable SCUs on non MSVC builds.

--- libevm/_libevm.cpp
+++ libevm/_libevm.cpp
@@ -1,4 +1,6 @@
+#ifdef _MSC_VER
 #include "All.h"
 #include "ExtVMFace.cpp"
 #include "FeeStructure.cpp"
 #include "VM.cpp"
+#endif
--- libwhisper/_libwhisper.cpp
+++ libwhisper/_libwhisper.cpp
@@ -1,3 +1,5 @@
+#ifdef _MSC_VER
 #include "All.h"
 #include "Common.cpp"
 #include "WhisperPeer.cpp"
+#endif
