c426f4bbba705ec41cc0600382329597b49e202c
arkpar <arkady.paronyan@gmail.com>
2014-10-08 01:39:49 +0400

    fixed upnp port mapping

--- libp2p/_libp2p.cpp
+++ libp2p/_libp2p.cpp
@@ -1,9 +1,11 @@
 #ifdef _MSC_VER
+#define ETH_MINIUPNPC 1
 #include "All.h"
 #include "Capability.cpp"
 #include "Common.cpp"
 #include "Host.cpp"
 #include "HostCapability.cpp"
+#undef clogS
 #include "Session.cpp"
 #include "UPnP.cpp"
 #endif
