51994f72a0410a91dbb2134150832e5e19518c4a
Gav Wood <i@gavwood.com>
2014-10-11 19:24:39 +0200

    Don't save peers with odd ports for now.

--- libp2p/Host.cpp
+++ libp2p/Host.cpp
@@ -687,7 +687,7 @@ bytes Host::saveNodes() const
 		for (auto const& i: m_nodes)
 		{
 			Node const& n = *(i.second);
-			if (!n.dead && n.id != id() && !isPrivateAddress(n.address.address()))
+			if (!n.dead && n.address.port() > 0 && n.address.port() >= 30300 && n.address.port() <= 30305 && n.id != id() && !isPrivateAddress(n.address.address()))
 			{
 				nodes.appendList(10);
 				if (n.address.address().is_v4())
