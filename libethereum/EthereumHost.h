443bc45ce60720940e7c2f075d79a5a1789cfbf8
Gav Wood <i@gavwood.com>
2014-10-14 12:35:06 +0300

    Move over to new GetTransactions semantics.

--- libethereum/EthereumHost.h
+++ libethereum/EthereumHost.h
@@ -84,7 +84,7 @@ private:
 	/// Sync with the BlockChain. It might contain one of our mined blocks, we might have new candidates from the network.
 	void doWork();
 
-	void maintainTransactions(h256 _currentBlock);
+	void maintainTransactions();
 	void maintainBlocks(h256 _currentBlock);
 
 	/// Get a bunch of needed blocks.
