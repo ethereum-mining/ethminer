d7c65beb3969e3ddf3ae69baadde75e0d3f885c2
subtly <subtly@users.noreply.github.com>
2014-10-18 05:32:19 +0200

    style, file headers

--- libdevcrypto/EC.h
+++ libdevcrypto/EC.h
@@ -1,16 +1,16 @@
 /*
  This file is part of cpp-ethereum.
- 
+
  cpp-ethereum is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  cpp-ethereum is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -18,7 +18,7 @@
  * @author Alex Leverington <nessence@gmail.com>
  * @date 2014
  *
- * Ethereum-specific data structures & algorithms.
+ * Shared EC classes and functions.
  */
 
 #pragma once
@@ -56,23 +56,22 @@ inline CryptoPP::OID secp256k1()
 {
 	return CryptoPP::ASN1::secp256k1();
 }
-	
+
 class ECKeyPair
 {
 public:
 	static ECKeyPair create();
-	CryptoPP::DL_PublicKey_EC<CryptoPP::ECP> pub() { return m_pub; } // deprecate
-	CryptoPP::DL_PrivateKey_EC<CryptoPP::ECP> sec() { return m_sec; } // deprecate
 	
+	/// deprecate
+	CryptoPP::DL_PublicKey_EC<CryptoPP::ECP> pub() { return m_pub; }
+	/// deprecate
+	CryptoPP::DL_PrivateKey_EC<CryptoPP::ECP> sec() { return m_sec; }
+
 private:
 	ECKeyPair() {}
 	CryptoPP::DL_PublicKey_EC<CryptoPP::ECP> m_pub;
 	CryptoPP::DL_PrivateKey_EC<CryptoPP::ECP> m_sec;
 };
 
-//class ECDHE;
-//bytes ECSign(KeyPair, bytesConstRef);
-//bool ECVerify(Public, bytesConstRef);
-	
 }
 }
