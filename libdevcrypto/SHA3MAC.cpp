e665c23c464daac25baa1140e12cd40366cdb496
subtly <subtly@users.noreply.github.com>
2014-10-27 17:21:01 +0100

    Require secret for hmac.

--- libdevcrypto/SHA3MAC.cpp
+++ libdevcrypto/SHA3MAC.cpp
@@ -31,8 +31,8 @@ using namespace CryptoPP;
 void crypto::sha3mac(bytesConstRef _secret, bytesConstRef _plain, bytesRef _output)
 {
 	CryptoPP::SHA3_256 ctx;
-	if (_secret.size() > 0)
-		ctx.Update((byte*)_secret.data(), _secret.size());
+	assert(_secret.size() > 0);
+	ctx.Update((byte*)_secret.data(), _secret.size());
 	ctx.Update((byte*)_plain.data(), _plain.size());
 	assert(_output.size() >= 32);
 	ctx.Final(_output.data());
