15013a3e8866052206a394fd04e396e757761a69
Gav Wood <i@gavwood.com>
2014-10-27 14:13:16 +0100

    PoC-7: Reversion of 0-hashes, empty-list hashes and sha3('') -> ''

--- libethereum/State.cpp
+++ libethereum/State.cpp
@@ -353,9 +353,9 @@ void State::ensureCached(std::map<Address, AddressState>& _cache, Address _a, bo
 		RLP state(stateBack);
 		AddressState s;
 		if (state.isNull())
-			s = AddressState(0, 0, h256(), EmptySHA3);
+			s = AddressState(0, 0, ZeroRLPSHA3, EmptySHA3);
 		else
-			s = AddressState(state[0].toInt<u256>(), state[1].toInt<u256>(), state[2].toHash<h256>(), state[3].isEmpty() ? EmptySHA3 : state[3].toHash<h256>());
+			s = AddressState(state[0].toInt<u256>(), state[1].toInt<u256>(), state[2].toHash<h256>(), state[3].toHash<h256>());
 		bool ok;
 		tie(it, ok) = _cache.insert(make_pair(_a, s));
 	}
