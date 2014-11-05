ff37ebcdb767d9dbe63af57bb71b213442204006
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-11-05 12:14:49 +0100

    miniupnpc cmake

---
+++ extdep/miniupnpc.cmake
@@ -0,0 +1,11 @@
+include(ExternalProject)
+
+ExternalProject_Add(miniupnpc
+	URL http://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-1.9.20141027.tar.gz 
+	BINARY_DIR miniupnpc-prefix/src/miniupnpc
+	CONFIGURE_COMMAND ""
+	BUILD_COMMAND make -j 3
+	INSTALL_COMMAND ""
+	)
+
+
