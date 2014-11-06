33368e31642aa388ea89a0c372b29e603351a2c0
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-11-06 11:49:40 +0100

    curl properly building to build dir

--- extdep/curl.cmake
+++ extdep/curl.cmake
@@ -1,11 +1,12 @@
-include(ExternalProject)
-
+if(${APPLE})
 ExternalProject_Add(curl
 	URL http://curl.haxx.se/download/curl-7.38.0.tar.bz2 
 	BINARY_DIR curl-prefix/src/curl
-	CONFIGURE_COMMAND ./configure --with-darwinssl
+	CONFIGURE_COMMAND ./configure --with-darwinssl --prefix=${ETH_DEPENDENCY_INSTALL_DIR} --exec-prefix=${ETH_DEPENDENCY_INSTALL_DIR}
 	BUILD_COMMAND make -j 3
-	INSTALL_COMMAND ""
+	INSTALL_COMMAND make install
 	)
+else()
 
+endif()
 
