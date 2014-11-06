a53b693d75ad1d96d2ebcf81ab8fa7fb2e5d9072
Christoph Jentzsch <jentzsch.software@gmail.com>
2014-11-06 17:47:03 +0100

    Added precompiledContracts tests

--- libethereum/Executive.cpp
+++ libethereum/Executive.cpp
@@ -87,7 +87,7 @@ bool Executive::setup(bytesConstRef _rlp)
 	if (startGasUsed + m_t.gas() > m_s.m_currentBlock.gasLimit)
 	{
 		clog(StateDetail) << "Too much gas used in this block: Require <" << (m_s.m_currentBlock.gasLimit - startGasUsed) << " Got" << m_t.gas();
-		BOOST_THROW_EXCEPTION(BlockGasLimitReached());
+		BOOST_THROW_EXCEPTION(BlockGasLimitReached() << RequirementError((int)(m_s.m_currentBlock.gasLimit - startGasUsed), (int)m_t.gas()));
 	}
 
 	// Increment associated nonce for sender.
