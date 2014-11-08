40ca8079f834d1135ed0935e692c4e25d7c38d18
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-11-08 22:58:34 +0100

    fix for web3.eth.accounts returning empty array

--- alethzero/MainWin.cpp
+++ alethzero/MainWin.cpp
@@ -610,6 +610,7 @@ void Main::readSettings(bool _skipGeometry)
 			}
 		}
 		ethereum()->setAddress(m_myKeys.back().address());
+		m_server->setAccounts(keysAsVector(m_myKeys));
 	}
 
 	{
@@ -1821,6 +1822,7 @@ void Main::on_send_clicked()
 void Main::keysChanged()
 {
 	onBalancesChange();
+	m_server->setAccounts(keysAsVector(m_myKeys));
 }
 
 void Main::on_debug_clicked()
