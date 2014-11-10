8ee3e56e0ae527fbd67dcb4a9a7adefb36a906aa
sveneh <sven@ethdev.com>
2014-11-10 19:14:01 +0100

    WIP: Cryptopp and json-rpc-cpp correctly downloading and compiling on Linux. Main cpp-ethereum buildfile finds dependencies, but does not compile yet

--- cmake/EthDependencies.cmake
+++ cmake/EthDependencies.cmake
@@ -1,11 +1,18 @@
 # all dependencies that are not directly included in the cpp-ethereum distribution are defined here
 # for this to work, download the dependency via the cmake script in extdep or install them manually!
 
-set(ETH_DEPENDENCY_INSTALL_DIR "${CMAKE_CURRENT_SOURCE_DIR}/extdep/install")
+# by defining this variable, cmake will look for dependencies first in our own repository before looking in system paths like /usr/local/ ...
+# this must be set to point to the same directory as $ETH_DEPENDENCY_INSTALL_DIR in /extdep directory
+set (CMAKE_FIND_ROOT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/extdep/install")
 
-set (CRYPTOPP_ROOT_DIR ${ETH_DEPENDENCY_INSTALL_DIR}) 	
 find_package (CryptoPP 5.6.2 REQUIRED)
-message("-- CryptoPP header: ${CRYPTOPP_INCLUDE_DIRS}")
-message("-- CryptoPP libs  : ${CRYPTOPP_LIBRARIES}")
+message(" - CryptoPP header: ${CRYPTOPP_INCLUDE_DIRS}")
+message(" - CryptoPP lib   : ${CRYPTOPP_LIBRARIES}")
 
 
+find_package (JsonRpcCpp REQUIRED)
+if (${JSON_RPC_CPP_FOUND})
+    message (" - json-rpc-cpp header: ${JSON_RPC_CPP_INCLUDE_DIRS}")
+    message (" - json-rpc-cpp lib   : ${JSON_RPC_CPP_LIBRARIES}")
+	add_definitions(-DETH_JSONRPC)
+endif()
