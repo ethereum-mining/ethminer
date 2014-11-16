2c340cd2c1b8a96063c01d9b3fbb6e3de0a8ceaf
sveneh <sven@ethdev.com>
2014-11-16 21:25:35 +0100

    Fixed issues caused by downgrading json-rpc-cpp to 0.2.1 introduced platform-specific direcory in extdep/install/<platform>

--- extdep/cryptopp.cmake
+++ extdep/cryptopp.cmake
@@ -1,18 +1,21 @@
-if(${APPLE})
-ExternalProject_Add(cryptopp
-     URL https://downloads.sourceforge.net/project/cryptopp/cryptopp/5.6.2/cryptopp562.zip
-     BINARY_DIR cryptopp-prefix/src/cryptopp
-     CONFIGURE_COMMAND ""
-     BUILD_COMMAND make CXX=clang++ CXXFLAGS=-DCRYPTOPP_DISABLE_ASM
-     INSTALL_COMMAND make install PREFIX=${ETH_DEPENDENCY_INSTALL_DIR}
+# CryptoPP does not have good cross-platform support, there exist several different other projects to make it work ... 
+
+if(APPLE)
+    ExternalProject_Add(cryptopp
+         URL https://downloads.sourceforge.net/project/cryptopp/cryptopp/5.6.2/cryptopp562.zip
+         BINARY_DIR cryptopp-prefix/src/cryptopp
+         CONFIGURE_COMMAND ""
+         BUILD_COMMAND make CXX=clang++ CXXFLAGS=-DCRYPTOPP_DISABLE_ASM
+         INSTALL_COMMAND make install PREFIX=${ETH_DEPENDENCY_INSTALL_DIR}
 )
+# on Linux, the default Makefile does not work.
 else()
-ExternalProject_Add(cryptopp
-	URL https://github.com/mmoss/cryptopp/archive/v5.6.2.zip
-	BINARY_DIR cryptopp-prefix/src/cryptopp
-	CONFIGURE_COMMAND ""
-	BUILD_COMMAND scons --shared --prefix=${ETH_DEPENDENCY_INSTALL_DIR}
-	INSTALL_COMMAND ""
+    ExternalProject_Add(cryptopp
+        URL https://github.com/mmoss/cryptopp/archive/v5.6.2.zip
+        BINARY_DIR cryptopp-prefix/src/cryptopp
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND scons --shared --prefix=${ETH_DEPENDENCY_INSTALL_DIR}
+        INSTALL_COMMAND ""
 )
 endif()
 	
