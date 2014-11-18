b7b388e8ac41ec1341d1dd872819d10d779c184c
sveneh <sven@ethdev.com>
2014-11-18 12:09:53 +0100

    changed the source repository for libjson-rpc-cpp AGAIN, as the forked one did not build on OS X. Back to the official repo again. Windows support we will have to do ourself.

--- extdep/json-rpc-cpp.cmake
+++ extdep/json-rpc-cpp.cmake
@@ -1,5 +1,5 @@
-# json-rpc-cpp is under heavy development, and multiplatform builds are not yet available. 
-# we use a forked repository which already has preliminary windows support
+# json-rpc-cpp is under heavy development, not yet stable, and multiplatform builds are not yet available. 
+# DO NOT MESS WITH THESE SETTINGS! IF YOU HAVE TO MAKE CHANGES HERE, CONSULT sven@ethdev.com BEFOREHAND!!
 
 if(APPLE)
 	set(CONFIG_CMD cmake -DCMAKE_INSTALL_PREFIX=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_MODULE_PATH:PATH=${CMAKE_CURRENT_SOURCE_DIR} -DETH_DEPENDENCY_INSTALL_DIR:PATH=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev .)
@@ -8,10 +8,11 @@ else()
 endif()
 
 
+# DO NOT CHANGE ANYTHING HERE!
 ExternalProject_Add(json-rpc-cpp
     # DEPENDS curl # re-enable later, when we build curl again
-	GIT_REPOSITORY https://github.com/gogo40/libjson-rpc-cpp.git
-	GIT_TAG 27f5da7a70c7a82b0614982cac829d6fd5fc8314 # this is roughly verson 0.3.2
+	GIT_REPOSITORY https://github.com/cinemast/libjson-rpc-cpp.git 
+	GIT_TAG v0.3.2
 	BINARY_DIR json-rpc-cpp-prefix/src/json-rpc-cpp
     CONFIGURE_COMMAND ${CONFIG_CMD}
 	BUILD_COMMAND make -j 3
