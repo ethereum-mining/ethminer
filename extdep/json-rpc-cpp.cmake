dd3ea58110bb49a4b9ff0b87b1515002ef63e6ba
sveneh <sven@ethdev.com>
2014-11-18 10:44:30 +0100

    switched to a fork of json-rpc-cpp, roughly resembling v0.3.2. This fork has already preliminary windows support

--- extdep/json-rpc-cpp.cmake
+++ extdep/json-rpc-cpp.cmake
@@ -1,21 +1,19 @@
-# json-rpc-cpp is under heavy development, and multiplatform builds are not yet available. All the platforms currently need patches to make them work.
+# json-rpc-cpp is under heavy development, and multiplatform builds are not yet available. 
+# we use a forked repository which already has preliminary windows support
 
 if(APPLE)
-    set(PATCH_CMD patch -d src/example < ${CMAKE_CURRENT_SOURCE_DIR}/json-rpc-cpp_osx.patch)
 	set(CONFIG_CMD cmake -DCMAKE_INSTALL_PREFIX=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_MODULE_PATH:PATH=${CMAKE_CURRENT_SOURCE_DIR} -DETH_DEPENDENCY_INSTALL_DIR:PATH=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev .)
 else()
-    set(PATCH_CMD patch --input=${CMAKE_CURRENT_SOURCE_DIR}/json-rpc-cpp_linux.patch --strip=1)
     set(CONFIG_CMD cmake -DCMAKE_INSTALL_PREFIX=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_MODULE_PATH:PATH=${CMAKE_CURRENT_SOURCE_DIR} -DETH_DEPENDENCY_INSTALL_DIR:PATH=${ETH_DEPENDENCY_INSTALL_DIR} -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST .)
 endif()
 
 
 ExternalProject_Add(json-rpc-cpp
-	DEPENDS curl
-	GIT_REPOSITORY https://github.com/cinemast/libjson-rpc-cpp.git 
-	GIT_TAG 0.2.1
+    # DEPENDS curl # re-enable later, when we build curl again
+	GIT_REPOSITORY https://github.com/gogo40/libjson-rpc-cpp.git
+	GIT_TAG 27f5da7a70c7a82b0614982cac829d6fd5fc8314 # this is roughly verson 0.3.2
 	BINARY_DIR json-rpc-cpp-prefix/src/json-rpc-cpp
-    PATCH_COMMAND ${PATCH_CMD}
     CONFIGURE_COMMAND ${CONFIG_CMD}
-	BUILD_COMMAND make jsonrpc -j 3
+	BUILD_COMMAND make -j 3
 	INSTALL_COMMAND make install
 )
