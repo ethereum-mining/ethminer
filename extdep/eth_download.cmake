f6e0699d5e61bb6276f1b0512623409ade547e9e
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-12-03 23:10:46 +0100

    leveldb precompiled dylib is working!

--- extdep/eth_download.cmake
+++ extdep/eth_download.cmake
@@ -8,6 +8,16 @@
 # eth_download("json-rpc-cpp")
 # eth_download("json-rpc-cpp" VERSION "0.3.2")
 #
+# params: 
+# VERSION - exact version we want to use
+# OSX_SCRIPT - script which will be executed on apple in install phase
+# UNIX_SCRIPT - script which will be executed on unix in install phase
+# WIN_SCRIPT - script which will be executed on win in install phase
+
+# OSX_SCRIPT, WIN_SCRIPT, UNIX_SCRIPT are taking 2 params:
+# $1 is package_source, 
+# $2 is ETH_DEPENDENCY_INSTALL_DIR
+#
 # parsing arguments
 # http://www.cmake.org/cmake/help/v3.0/module/CMakeParseArguments.html
 #
@@ -47,7 +57,8 @@ macro(eth_download eth_package_name)
 	endif()
 
 	if (eth_package_install)
-		message(STATUS "install script is at: ${eth_package_install}")
+		message(STATUS "install script: ${eth_package_install}")
+		set (eth_package_install ${eth_package_install} . ${ETH_DEPENDENCY_INSTALL_DIR})
 	else ()
 		set (eth_package_install echo 0) # cause empty string is not handled properly
 	endif()
@@ -57,7 +68,7 @@ macro(eth_download eth_package_name)
 		BINARY_DIR ${eth_package_name}-prefix/src/${eth_package_name}
 		CONFIGURE_COMMAND ""
 		BUILD_COMMAND ${eth_package_copy}
-		INSTALL_COMMAND ${eth_package_install} . ${ETH_DEPENDENCY_INSTALL_DIR}
+		INSTALL_COMMAND ${eth_package_install}
 	)
 endmacro()
 
