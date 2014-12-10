ddd180f9a55c0095b15cccde2ca72f8eb7aacfa2
ethdev <marek.kotewicz@gmail.com>
2014-12-10 16:58:05 +0100

    windows boost compiling with program_options module

--- extdep/compile/boost.cmake
+++ extdep/compile/boost.cmake
@@ -5,7 +5,7 @@ set(boost_address_model)
 # on windows 64:
 # set(boost_address_model address-model=64)
 
-set(boost_targets --with-filesystem --with-system --with-thread --with-date_time --with-regex --with-test --with-chrono)
+set(boost_targets --with-filesystem --with-system --with-thread --with-date_time --with-regex --with-test --with-chrono --with-program_options)
 ExternalProject_Add(boost
 	URL http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.gz
 	BINARY_DIR boost-prefix/src/boost
