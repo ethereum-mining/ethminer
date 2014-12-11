f6e0c6a7f68ce4f11723a9351764394917aff67c
yann300 <yann.levreau@gmai.com>
2014-12-11 11:48:13 +0100

    add QContractDefinition, QBasicNodeDefinition, QFunctionDefinition, QVariableDeclaration

---
+++ mix/QBasicNodeDefinition.h
@@ -0,0 +1,48 @@
+/*
+	This file is part of cpp-ethereum.
+
+	cpp-ethereum is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	cpp-ethereum is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** @file QBasicNodeDefinition.h
+ * @author Yann yann@ethdev.com
+ * @date 2014
+ */
+
+#pragma once
+
+#include <QObject>
+#include "libsolidity/AST.h"
+
+namespace dev
+{
+namespace mix
+{
+
+class QBasicNodeDefinition: public QObject
+{
+	Q_OBJECT
+	Q_PROPERTY(QString name READ name)
+
+public:
+	QBasicNodeDefinition(): QObject() {}
+	~QBasicNodeDefinition() {}
+	QBasicNodeDefinition(std::shared_ptr<dev::solidity::Declaration> _d, QObject* _parent): QObject(_parent), m_dec(_d) {}
+	QString name() { return QString::fromStdString(m_dec.get()->getName()); }
+
+protected:
+	std::shared_ptr<dev::solidity::Declaration>  m_dec;
+};
+
+}
+}
