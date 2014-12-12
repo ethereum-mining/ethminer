b121491080c60a9c3752f38d23af6e302ca17558
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-12-12 12:40:59 +0100

    removed walleth, libpyserpent, iethxi and QmlEthereum class

--- iethxi/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QtQml/QQmlApplicationEngine>
-#include <QtWidgets/QApplication>
-
-int main(int argc, char *argv[])
-{
-	QApplication a(argc, argv);
-	QQmlApplicationEngine app(QUrl("qrc:/Simple.qml"));
-	return a.exec();
-}
