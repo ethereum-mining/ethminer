f609185a0e8fe555f7e5335ed0f133f594779a9b
Gav Wood <i@gavwood.com>
2014-12-21 17:44:44 +0100

    Allow interaction with PV49. Remove unneeded include.

--- libethcore/CommonEth.cpp
+++ libethcore/CommonEth.cpp
@@ -32,7 +32,7 @@ namespace dev
 namespace eth
 {
 
-const unsigned c_protocolVersion = 51;
+const unsigned c_protocolVersion = 49;
 const unsigned c_databaseVersion = 5;
 
 static const vector<pair<u256, string>> g_units =
