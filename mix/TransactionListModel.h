188319acf1dfb8b203bd536ce39737612a95b6b5
arkpar <arkadiy@ethdev.com>
2014-12-22 12:16:57 +0100

    removed unnessary casts, fixed geteItem return type

--- mix/TransactionListModel.h
+++ mix/TransactionListModel.h
@@ -143,7 +143,7 @@ public:
 	/// @todo Change that to transaction item
 	Q_INVOKABLE void edit(QObject* _data);
 	/// @returns transaction item for a give index
-	Q_INVOKABLE QObject* getItem(int _index);
+	Q_INVOKABLE TransactionListItem* getItem(int _index);
 	/// @returns a list of functions for current contract
 	Q_INVOKABLE QList<QString> getFunctions();
 	/// @returns function parameters along with parameter values if set. @see TransactionParameterItem
