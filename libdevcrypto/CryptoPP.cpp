7775503c168bf9ac2f76e9133a3ce45398004b93
Gav Wood <i@gavwood.com>
2015-01-13 12:20:07 +0000

    Warnings fixes.

--- libdevcrypto/CryptoPP.cpp
+++ libdevcrypto/CryptoPP.cpp
@@ -198,6 +198,9 @@ bool Secp256k1::verifySecret(Secret const& _s, Public& _p)
 
 void Secp256k1::agree(Secret const& _s, Public const& _r, h256& o_s)
 {
+	(void)_s;
+	(void)_r;
+	(void)o_s;
 	ECDH<ECP>::Domain d(m_oid);
 	assert(d.AgreedValueLength() == sizeof(o_s));
 	byte remote[65] = {0x04};
