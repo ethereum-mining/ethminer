5eabe8e1c6b3f1ddc0a1a8c5b77800634741f103
winsvega <winsvega@mail.ru>
2015-01-23 02:48:15 +0300

    transaction tests, bound tests
    Former-commit-id: f07a49b02e4235332afdb6d4c80f7912b234a3b3

--- libdevcrypto/Common.h
+++ libdevcrypto/Common.h
@@ -51,7 +51,7 @@ struct SignatureStruct
 	operator Signature() const { return *(h520 const*)this; }
 
 	/// @returns true if r,s,v values are valid, otherwise false
-    bool isValid() const;
+	bool isValid() const;
 
 	h256 r;
 	h256 s;
