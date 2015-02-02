42d72fe8a4e7d1aa9dfb8d2ca8b83dcc2b9f2af2
Paweł Bylica <pawel.bylica@imapp.pl>
2015-02-02 17:08:57 +0100

    Count gas using int64
    Former-commit-id: d570ffb842030aa1a21eaf272c47ef83c92a5121

--- evmjit/libevmjit/GasMeter.h
+++ evmjit/libevmjit/GasMeter.h
@@ -50,7 +50,7 @@ public:
 private:
 	/// Cumulative gas cost of a block of instructions
 	/// @TODO Handle overflow
-	uint64_t m_blockCost = 0;
+	int64_t m_blockCost = 0;
 
 	llvm::CallInst* m_checkCall = nullptr;
 	llvm::Function* m_gasCheckFunc = nullptr;
