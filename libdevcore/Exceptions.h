ce3638f75dd90b90e0d889975182ca6f1238e4a4
winsvega <winsvega@mail.ru>
2015-02-10 13:07:31 +0300

    Test related Constant in TestHelper.h Exception in TestHelper.h

--- libdevcore/Exceptions.h
+++ libdevcore/Exceptions.h
@@ -33,7 +33,6 @@ namespace dev
 // base class for all exceptions
 struct Exception: virtual std::exception, virtual boost::exception { mutable std::string m_message; };
 
-struct ValueTooLarge: virtual Exception {};
 struct BadHexCharacter: virtual Exception {};
 struct RLPException: virtual Exception {};
 struct BadCast: virtual RLPException {};
