40c8ec7cb6f9bba65db0e9e903f5fa0d840cbc41
Gav Wood <i@gavwood.com>
2015-03-02 15:30:22 +0100

    New gas pricing for PoC-9.

--- libevm/FeeStructure.cpp
+++ libevm/FeeStructure.cpp
@@ -25,24 +25,41 @@ using namespace std;
 using namespace dev;
 using namespace dev::eth;
 
-u256 const dev::eth::c_stepGas = 1;
-u256 const dev::eth::c_balanceGas = 20;
-u256 const dev::eth::c_sha3Gas = 10;
-u256 const dev::eth::c_sha3WordGas = 10;
-u256 const dev::eth::c_sloadGas = 20;
-u256 const dev::eth::c_sstoreSetGas = 300;
-u256 const dev::eth::c_sstoreResetGas = 100;
-u256 const dev::eth::c_sstoreRefundGas = 100;
-u256 const dev::eth::c_createGas = 100;
-u256 const dev::eth::c_createDataGas = 5;
-u256 const dev::eth::c_callGas = 20;
-u256 const dev::eth::c_expGas = 1;
-u256 const dev::eth::c_expByteGas = 1;
-u256 const dev::eth::c_memoryGas = 1;
-u256 const dev::eth::c_txDataZeroGas = 1;
-u256 const dev::eth::c_txDataNonZeroGas = 5;
-u256 const dev::eth::c_txGas = 500;
-u256 const dev::eth::c_logGas = 32;
-u256 const dev::eth::c_logDataGas = 1;
-u256 const dev::eth::c_logTopicGas = 32;
-u256 const dev::eth::c_copyGas = 1;
+u256 const dev::eth::c_tierStepGas[8] = { 0, 2, 3, 5, 8, 10, 20, 0 };
+u256 const dev::eth::c_expGas = 10;
+u256 const dev::eth::c_expByteGas = 10;
+
+u256 const dev::eth::c_sha3Gas = 30;
+u256 const dev::eth::c_sha3WordGas = 6;
+
+u256 const dev::eth::c_sloadGas = 50;
+u256 const dev::eth::c_sstoreSetGas = 20000;
+u256 const dev::eth::c_sstoreResetGas = 5000;
+u256 const dev::eth::c_sstoreClearGas = 5000;
+u256 const dev::eth::c_sstoreRefundGas = 15000;
+u256 const dev::eth::c_jumpdestGas = 1;
+
+u256 const dev::eth::c_logGas = 2000;
+u256 const dev::eth::c_logDataGas = 8;
+u256 const dev::eth::c_logTopicGas = 2000;
+
+u256 const dev::eth::c_createGas = 32000;
+
+u256 const dev::eth::c_callGas = 40;
+u256 const dev::eth::c_callValueTransferGas = 6700;
+u256 const dev::eth::c_callNewAccountGas = 25000;
+
+u256 const dev::eth::c_suicideRefundGas = 24000;
+
+u256 const dev::eth::c_memoryGas = 3;
+u256 const dev::eth::c_quadCoeffDiv = 512;
+
+
+u256 const dev::eth::c_createDataGas = 200;
+u256 const dev::eth::c_txGas = 21000;
+u256 const dev::eth::c_txDataZeroGas = 37;
+u256 const dev::eth::c_txDataNonZeroGas = 2;
+
+u256 const dev::eth::c_copyGas = 3;
+
+
