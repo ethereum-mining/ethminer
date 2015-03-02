58d5e2b27b53046181daf3509ffde4f3cb9829a1
Gav Wood <i@gavwood.com>
2015-03-02 17:27:10 +0100

    Gas fixes. Trie fixes.
    Former-commit-id: de177effb08484bfabd2060f5b7619a862ac8b77

--- libdevcrypto/OverlayDB.cpp
+++ libdevcrypto/OverlayDB.cpp
@@ -52,14 +52,15 @@ void OverlayDB::commit()
 			if (m_refCount[i.first])
 				m_db->Put(m_writeOptions, ldb::Slice((char const*)i.first.data(), i.first.size), ldb::Slice(i.second.data(), i.second.size()));
 		}
-		if (m_auxKey && m_aux.count(m_auxKey))
-		{
-			m_db->Put(m_writeOptions, m_auxKey.ref(), bytesConstRef(&m_aux[m_auxKey]));
-			cdebug << "Committing aux: " << m_auxKey;
-			m_aux.erase(m_auxKey);
-			cdebug << "Discarding " << keysOf(m_aux);
-		}
-		m_auxKey = h256();
+		for (auto const& i: m_auxActive)
+			if (m_aux.count(i))
+			{
+				m_db->Put(m_writeOptions, i.ref(), bytesConstRef(&m_aux[i]));
+				cdebug << "Committing aux: " << i;
+				m_aux.erase(i);
+			}
+		cdebug << "Discarding " << keysOf(m_aux);
+		m_auxActive.clear();
 		m_aux.clear();
 		m_over.clear();
 		m_refCount.clear();
