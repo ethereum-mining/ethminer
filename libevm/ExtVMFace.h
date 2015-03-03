21d271a17c0330e6bf37338e2a494c141da874e8
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-03-03 15:56:49 +0100

    fixing pr issues
    Former-commit-id: 148e2d8d14bac845bf2eaf4e3cb25610183df2d1

--- libevm/ExtVMFace.h
+++ libevm/ExtVMFace.h
@@ -63,7 +63,7 @@ using LogEntries = std::vector<LogEntry>;
 struct LocalisedLogEntry: public LogEntry
 {
 	LocalisedLogEntry() {}
-	LocalisedLogEntry(LogEntry const& _le, unsigned _number, h256 _sha3 = h256(u256(0))): LogEntry(_le), number(_number), sha3(_sha3) {}
+	LocalisedLogEntry(LogEntry const& _le, unsigned _number, h256 _sha3 = {}): LogEntry(_le), number(_number), sha3(_sha3) {}
 
 	unsigned number = 0;
 	h256 sha3;
