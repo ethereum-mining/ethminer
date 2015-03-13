69e1902aa52469de7ee6cfb62c6779fd4af25fd1
CJentzsch <jentzsch.software@gmail.com>
2015-03-13 12:29:59 +0100

    fix calldataload

--- libevm/VM.cpp
+++ libevm/VM.cpp
@@ -341,8 +341,12 @@ bytesConstRef VM::go(ExtVMFace& _ext, OnOpFunc const& _onOp, uint64_t _steps)
 			break;
 		case Instruction::CALLDATALOAD:
 		{
-			if ((unsigned)m_stack.back() + (uint64_t)31 < _ext.data.size())
-				m_stack.back() = (u256)*(h256 const*)(_ext.data.data() + (unsigned)m_stack.back());
+			if ((bigint)m_stack.back() + 31 < _ext.data.size())
+				m_stack.back() = (u256)*(h256 const*)(_ext.data.data() + (size_t)m_stack.back());
+			else if ((bigint)m_stack.back() >= _ext.data.size())
+			{
+				m_stack.back() = u256();
+			}
 			else
 			{
 				h256 r;
