f5ba0d6ca14b59a96cde8145434c4e448346eada
subtly <subtly@users.noreply.github.com>
2015-03-22 18:33:15 +0100

    constant for discovery protocol version
    Former-commit-id: 4a8d443d491b72d7efe672fe6f2597fa91a5bb7f

--- libp2p/Host.cpp
+++ libp2p/Host.cpp
@@ -709,7 +709,7 @@ bytes Host::saveNetwork() const
 	}
 
 	RLPStream ret(3);
-	ret << 2 << m_alias.secret();
+	ret << NodeTable::s_version << m_alias.secret();
 	ret.appendList(count).appendRaw(network.out(), count);
 	return ret.out();
 }
@@ -722,7 +722,7 @@ void Host::restoreNetwork(bytesConstRef _b)
 
 	RecursiveGuard l(x_sessions);
 	RLP r(_b);
-	if (r.itemCount() > 0 && r[0].isInt() && r[0].toInt<int>() == 2)
+	if (r.itemCount() > 0 && r[0].isInt() && r[0].toInt<int>() == NodeTable::s_version)
 	{
 		// r[0] = version
 		// r[1] = key
