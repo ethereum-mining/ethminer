51a4dac8f0714033b409280ec47531b029648408
subtly <subtly@users.noreply.github.com>
2015-03-29 22:10:34 +0200

    style
    Former-commit-id: 51431e49892152583866f521f11697051d20dd55

--- alethzero/Connect.cpp
+++ alethzero/Connect.cpp
@@ -1,18 +1,18 @@
 /*
-    This file is part of cpp-ethereum.
+	This file is part of cpp-ethereum.
 
-    cpp-ethereum is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	cpp-ethereum is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    cpp-ethereum is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	cpp-ethereum is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
 /** @file Connect.cpp
  * @author Alex Leverington <nessence@gmail.com>
@@ -25,15 +25,15 @@
 #include "ui_Connect.h"
 
 Connect::Connect(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::Connect)
+	QDialog(parent),
+	ui(new Ui::Connect)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 }
 
 Connect::~Connect()
 {
-    delete ui;
+	delete ui;
 }
 
 void Connect::setEnvironment(QStringList const& _nodes)
