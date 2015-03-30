206bfe29cf9dd631a5efc5b85e2f069f3accd714
Lefteris Karapetsas <lefteris@refu.co>
2015-03-30 16:43:23 +0200

    Fixing build for msvc

--- io.h
+++ io.h
@@ -32,7 +32,8 @@ typedef struct ethash_blockhash { uint8_t b[32]; } ethash_blockhash_t;
 
 static const char DAG_FILE_NAME[] = "full";
 static const char DAG_MEMO_NAME[] = "full.info";
-static const unsigned int DAG_MEMO_BYTESIZE = 36;
+// MSVC thinks that "static const unsigned int" is not a compile time variable. Sorry for the #define :(
+#define DAG_MEMO_BYTESIZE 36
 
 /// Possible return values of @see ethash_io_prepare
 enum ethash_io_rc {
