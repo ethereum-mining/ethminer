98a2805b5c399dba5cbb77b56967c35457218182
ethdev zug <marek.kotewicz@gmail.com>
2015-04-13 17:35:29 +0100

    fixed copying dlls

--- cmake/scripts/copydlls.cmake
+++ cmake/scripts/copydlls.cmake
@@ -1,16 +1,17 @@
 # this module expects
 # DLLS
-# CONFIGURATION
+# CONF
 # DESTINATION
 
 # example usage:
-# cmake -DLLS=${MHD_LIBRARIES} -DCONFIGURATION=Release -DDESTINATION=dest -P scripts/copydlls.cmake
+# cmake -DDLL_DEBUG=xd.dll -DDLL_RELEASE=x.dll -DCONFIGURATION=Release -DDESTINATION=dest -P scripts/copydlls.cmake
 
-# expects DLLS to be in format optimized;path_to_dll.dll;debug;path_to_dll_d.dll
-if (${CONFIGURATION} STREQUAL "Release")
-    list(GET DLLS 1 DLL)
+# this script is created cause we do not know configuration in multiconfiguration generators at cmake configure phase ;)
+
+if ("${CONF}" STREQUAL "Release")
+    set(DLL ${DLL_RELEASE})
 else () # Debug
-    list(GET DLLS 3 DLL)
+    set(DLL ${DLL_DEBUG})
 endif()
 
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${DLL}" "${DESTINATION}")
