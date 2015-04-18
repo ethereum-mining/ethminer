e68e556307a02ccedfdaa0b7776bde1c0e0d578f
Gav Wood <i@gavwood.com>
2015-04-19 00:51:04 +0200

    Don't create full if missing when just verifying.
    Former-commit-id: 110da0176b9ebeb2813024724d0912447bd25074

--- libethcore/EthashAux.h
+++ libethcore/EthashAux.h
@@ -60,8 +60,8 @@ public:
 	static ethash_params params(unsigned _n);
 	static LightType light(BlockInfo const& _header);
 	static LightType light(h256 const& _seedHash);
-	static FullType full(BlockInfo const& _header, bytesRef _dest = bytesRef());
-	static FullType full(h256 const& _header, bytesRef _dest = bytesRef());
+	static FullType full(BlockInfo const& _header, bytesRef _dest = bytesRef(), bool _createIfMissing = true);
+	static FullType full(h256 const& _header, bytesRef _dest = bytesRef(), bool _createIfMissing = true);
 
 	static Ethash::Result eval(BlockInfo const& _header) { return eval(_header, _header.nonce); }
 	static Ethash::Result eval(BlockInfo const& _header, Nonce const& _nonce);
