1c66c63b463e49a3596c7bc62f6480c70ff736b7
Paweł Bylica <pawel.bylica@imapp.pl>
2015-04-21 16:23:01 +0200

    Remove some LLVM 3.5 bugs workarounds
    Former-commit-id: 56c3d1858201a3956ce669b03875ebac060e9b3e

--- evmjit/libevmjit/Compiler.cpp
+++ evmjit/libevmjit/Compiler.cpp
@@ -464,12 +464,8 @@ void Compiler::compileBasicBlock(BasicBlock& _basicBlock, RuntimeManager& _runti
 			// test for word >> (k * 8 + 7)
 			auto bitpos = m_builder.CreateAdd(k32x8, m_builder.getInt64(7), "bitpos");
 			auto bitposEx = m_builder.CreateZExt(bitpos, Type::Word);
-			auto bittester = m_builder.CreateShl(Constant::get(1), bitposEx);
-			auto bitresult = m_builder.CreateAnd(word, bittester);
-			auto bittest = m_builder.CreateICmpUGT(bitresult, Constant::get(0));
-			// FIXME: The following does not work - LLVM bug, report!
-			//auto bitval = m_builder.CreateLShr(word, bitpos, "bitval");
-			//auto bittest = m_builder.CreateTrunc(bitval, Type::Bool, "bittest");
+			auto bitval = m_builder.CreateLShr(word, bitposEx, "bitval");
+			auto bittest = m_builder.CreateTrunc(bitval, Type::Bool, "bittest");
 
 			auto mask_ = m_builder.CreateShl(Constant::get(1), bitposEx);
 			auto mask = m_builder.CreateSub(mask_, Constant::get(1), "mask");
