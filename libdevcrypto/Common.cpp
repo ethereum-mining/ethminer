99225a993ba0519f8b9442fbac78fa536f59f514
winsvega <winsvega@mail.ru>
2015-04-23 20:17:49 +0300

    issues: right r<1, s<1 comparation
    Former-commit-id: d93c32462b08c0889f650631efb4c6452bac7f2e

--- libdevcrypto/Common.cpp
+++ libdevcrypto/Common.cpp
@@ -40,8 +40,8 @@ bool dev::SignatureStruct::isValid() const
 	if (v > 1 ||
 		r >= h256("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141") ||
 		s >= h256("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141") ||
-		s < h256("0x01") ||
-		r < h256("0x01"))
+		s < h256("0x0000000000000000000000000000000000000000000000000000000000000001") ||
+		r < h256("0x0000000000000000000000000000000000000000000000000000000000000001"))
 		return false;
 	return true;
 }
