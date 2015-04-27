45e10279b32e0277a07cacfdb45678e1d8f40706
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-28 00:43:37 +0200

    libjsconsole init
    Former-commit-id: 41afe1672ecd2baa9f49621d9b2f02a2eeb76d8e

---
+++ libjsconsole/JSV8Console.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Marek Kotewicz on 28/04/15.
+//
+
+#include <libjsengine/JSV8Engine.h>
+#include <libjsengine/JSV8Printer.h>
+#include "JSV8Console.h"
+
+using namespace dev;
+using namespace dev::eth;
+
+JSV8Console::JSV8Console(): m_engine(), m_printer(m_engine)
+{
+
+}
+
+void JSV8Console::repl() const
+{
+
+}
