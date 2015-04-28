463025e15de5181a9661e6e67876588908bb8eb9
Paweł Bylica <pawel.bylica@imapp.pl>
2015-04-28 17:50:01 +0200

    Implementation of MUL workaround in "LLVM pass" way.

--- libevmjit/Optimizer.cpp
+++ libevmjit/Optimizer.cpp
@@ -1,10 +1,15 @@
 #include "Optimizer.h"
 
 #include "preprocessor/llvm_includes_start.h"
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Function.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Transforms/Scalar.h>
 #include <llvm/Transforms/IPO.h>
 #include "preprocessor/llvm_includes_end.h"
+
+#include "Arith256.h"
+#include "Type.h"
 
 namespace dev
 {
@@ -21,6 +26,60 @@ bool optimize(llvm::Module& _module)
 	pm.add(llvm::createInstructionCombiningPass());
 	pm.add(llvm::createAggressiveDCEPass());
 	pm.add(llvm::createLowerSwitchPass());
+	return pm.run(_module);
+}
+
+namespace
+{
+
+class LowerEVMPass : public llvm::BasicBlockPass
+{
+	static char ID;
+
+	bool m_mulFuncNeeded = false;
+
+public:
+	LowerEVMPass():
+		llvm::BasicBlockPass(ID)
+	{}
+
+	virtual bool runOnBasicBlock(llvm::BasicBlock& _bb) override;
+
+	virtual bool doFinalization(llvm::Module& _module) override;
+};
+
+char LowerEVMPass::ID = 0;
+
+bool LowerEVMPass::runOnBasicBlock(llvm::BasicBlock& _bb)
+{
+	auto modified = false;
+	auto module = _bb.getParent()->getParent();
+	for (auto&& inst : _bb)
+	{
+		if (inst.getOpcode() == llvm::Instruction::Mul)
+		{
+			if (inst.getType() == Type::Word)
+			{
+				auto call = llvm::CallInst::Create(Arith256::getMulFunc(*module), {inst.getOperand(0), inst.getOperand(1)}, "", &inst);
+				inst.replaceAllUsesWith(call);
+				modified = true;
+			}
+		}
+	}
+	return modified;
+}
+
+bool LowerEVMPass::doFinalization(llvm::Module&)
+{
+	return false;
+}
+
+}
+
+bool prepare(llvm::Module& _module)
+{
+	auto pm = llvm::legacy::PassManager{};
+	pm.add(new LowerEVMPass{});
 	return pm.run(_module);
 }
 
