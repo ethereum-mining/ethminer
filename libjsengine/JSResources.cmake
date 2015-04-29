d78c5f7e077b0a28bb169ca2ff31dd6e1e66fff3
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-30 01:40:22 +0200

    pretty printing in js console in progress
    Former-commit-id: 1f4d0e32bf46c3aa3cfa9c8e1bfb012af3dc8eaf

---
+++ libjsengine/JSResources.cmake
@@ -0,0 +1,7 @@
+
+set(pretty_print "${CMAKE_CURRENT_LIST_DIR}/PrettyPrint.js")
+
+set(ETH_RESOURCE_NAME "JSEngineResources")
+set(ETH_RESOURCE_LOCATION "${CMAKE_CURRENT_BINARY_DIR}")
+set(ETH_RESOURCES "pretty_print")
+
