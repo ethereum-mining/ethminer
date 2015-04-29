1f4d0e32bf46c3aa3cfa9c8e1bfb012af3dc8eaf
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-30 01:40:22 +0200

    pretty printing in js console in progress

--- libjsengine/JSV8Printer.cpp
+++ libjsengine/JSV8Printer.cpp
@@ -2,13 +2,19 @@
 // Created by Marek Kotewicz on 28/04/15.
 //
 
+#include <string>
 #include "JSV8Printer.h"
+#include "libjsengine/JSEngineResources.hpp"
 
+using namespace std;
 using namespace dev;
 using namespace eth;
 
 JSV8Printer::JSV8Printer(JSV8Engine const& _engine)
 {
+	JSEngineResources resources;
+	string prettyPrint = resources.loadResourceAsString("pretty_print");
+	_engine.eval(prettyPrint.c_str());
 }
 
 const char* JSV8Printer::prettyPrint(JSV8Value const& _value) const
