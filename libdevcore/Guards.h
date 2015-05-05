62e3be8666d741775861c73a08612b9e1cffd670
arkpar <arkady.paronyan@gmail.com>
2015-05-05 13:29:10 +0200

    replaces cache maps with hash tables, reverted noteCanonChanged
    Former-commit-id: 7387a1c250aec34bcc978b9ea48c2d109bb31271

--- libdevcore/Guards.h
+++ libdevcore/Guards.h
@@ -66,11 +66,11 @@ struct GenericUnguardSharedBool
 class SpinLock
 {
 public:
-	SpinLock() { lck.clear(); }
-	void lock() { while (lck.test_and_set(std::memory_order_acquire)) {} }
-	void unlock() { lck.clear(std::memory_order_release); }
+	SpinLock() { m_lock.clear(); }
+	void lock() { while (m_lock.test_and_set(std::memory_order_acquire)) {} }
+	void unlock() { m_lock.clear(std::memory_order_release); }
 private:
-	std::atomic_flag lck;
+	std::atomic_flag m_lock;
 };
 using SpinGuard = std::lock_guard<SpinLock>;
 
