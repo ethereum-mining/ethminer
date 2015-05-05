bed7c62bca4c4e420337865fa3f32d7633ad0ade
Lefteris Karapetsas <lefteris@refu.co>
2015-05-05 14:54:24 +0200

    Fixes for building on Mac

--- sha3_cryptopp.h
+++ sha3_cryptopp.h
@@ -9,9 +9,8 @@ extern "C" {
 #endif
 
 struct ethash_h256;
-typedef struct ethash_h256 ethash_h256_t;
 
-void SHA3_256(ethash_h256_t const* ret, uint8_t const* data, size_t size);
+void SHA3_256(struct ethash_h256 const* ret, uint8_t const* data, size_t size);
 void SHA3_512(uint8_t* const ret, uint8_t const* data, size_t size);
 
 #ifdef __cplusplus
