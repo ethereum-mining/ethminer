c9454b616a60f0d476333b655458c81830562f9a
Gustav Simonsson <gustav.simonsson@gmail.com>
2015-05-07 01:17:27 +0200

    Add comment about stack probing and remove unused ifdefs

--- util.h
+++ util.h
@@ -26,11 +26,7 @@
 extern "C" {
 #endif
 
-//#ifdef _MSC_VER
 void debugf(char const* str, ...);
-//#else
-//#define debugf printf
-//#endif
 
 static inline uint32_t min_u32(uint32_t a, uint32_t b)
 {
