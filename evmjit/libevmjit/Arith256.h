0fc69aad6019e2c5683d5cbc0c85d1aa9b86ea98
Paweł Bylica <pawel.bylica@imapp.pl>
2015-05-13 18:55:14 +0200

    Lower ADDMOD & MULMOD (limited) to a function call in the LLVM pass after optimization.
    Former-commit-id: 3b8e2996a5db4ad5704f22fe20478337dc40376a

--- evmjit/libevmjit/Arith256.h
+++ evmjit/libevmjit/Arith256.h
@@ -15,12 +15,11 @@ public:
 	Arith256(llvm::IRBuilder<>& _builder);
 
 	llvm::Value* exp(llvm::Value* _arg1, llvm::Value* _arg2);
-	llvm::Value* mulmod(llvm::Value* _arg1, llvm::Value* _arg2, llvm::Value* _arg3);
-	llvm::Value* addmod(llvm::Value* _arg1, llvm::Value* _arg2, llvm::Value* _arg3);
 
 	void debug(llvm::Value* _value, char _c);
 
 	static llvm::Function* getMulFunc(llvm::Module& _module);
+	static llvm::Function* getMul512Func(llvm::Module& _module);
 	static llvm::Function* getUDiv256Func(llvm::Module& _module);
 	static llvm::Function* getURem256Func(llvm::Module& _module);
 	static llvm::Function* getURem512Func(llvm::Module& _module);
@@ -31,15 +30,9 @@ public:
 	static llvm::Function* getUDivRem512Func(llvm::Module& _module);
 
 private:
-	llvm::Function* getMul512Func();
 	llvm::Function* getExpFunc();
-	llvm::Function* getAddModFunc();
-	llvm::Function* getMulModFunc();
 
-	llvm::Function* m_mul512 = nullptr;
 	llvm::Function* m_exp = nullptr;
-	llvm::Function* m_addmod = nullptr;
-	llvm::Function* m_mulmod = nullptr;
 	llvm::Function* m_debug = nullptr;
 };
 
