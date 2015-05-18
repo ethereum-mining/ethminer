3d4d5942e7c9c5df528a80918f663e6aa0006043
Gav Wood <i@gavwood.com>
2015-05-18 13:16:46 +0200

    Bump and build fix.
    Former-commit-id: 45c030af45ee765947bd30448bb840cd5c622d21

--- libethcore/EthashAux.cpp
+++ libethcore/EthashAux.cpp
@@ -242,6 +242,6 @@ Ethash::Result EthashAux::eval(h256 const& _seedHash, h256 const& _headerHash, N
 		return dag->compute(_headerHash, _nonce);
 	DEV_IF_THROWS(return EthashAux::get()->light(_seedHash)->compute(_headerHash, _nonce))
 	{
-		return Ethash::Result(~h256(), h256());
+		return Ethash::Result{ ~h256(), h256() };
 	}
 }
