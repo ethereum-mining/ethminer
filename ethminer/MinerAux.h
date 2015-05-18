dd8bc4b78d30a97536001ab8fd37c7b1ac194b04
Gav Wood <i@gavwood.com>
2015-05-18 21:00:21 +0200

    Parallelised uncle checking.

--- ethminer/MinerAux.h
+++ ethminer/MinerAux.h
@@ -362,7 +362,7 @@ private:
 		cout << "inner mean: " << innerMean << " H/s" << endl;
 
 		(void)_phoneHome;
-	#if ETH_JSONRPC || !ETH_TRUE
+#if ETH_JSONRPC || !ETH_TRUE
 		if (_phoneHome)
 		{
 			cout << "Phoning home to find world ranking..." << endl;
@@ -378,7 +378,7 @@ private:
 				cout << "Error phoning home. ET is sad." << endl;
 			}
 		}
-	#endif
+#endif
 		exit(0);
 	}
 
