846de9aceaca7fae862c2fbe7b6fb5612e80a5b2
Gav Wood <i@gavwood.com>
2015-05-20 09:31:38 +0200

    Pregenerate DAG when mining on AZ/eth/ethminer. Use regeneratable key for decrets by default.

--- libethcore/Ethash.cpp
+++ libethcore/Ethash.cpp
@@ -75,6 +75,13 @@ Ethash::WorkPackage Ethash::package(BlockInfo const& _bi)
 	return ret;
 }
 
+void Ethash::ensurePrecomputed(unsigned _number)
+{
+	if (_number % ETHASH_EPOCH_LENGTH > ETHASH_EPOCH_LENGTH * 9 / 10)
+		// 90% of the way to the new epoch
+		EthashAux::computeFull(EthashAux::seedHash(_number + ETHASH_EPOCH_LENGTH), true);
+}
+
 void Ethash::prep(BlockInfo const& _header, std::function<int(unsigned)> const& _f)
 {
 	EthashAux::full(_header.seedHash(), true, _f);
@@ -306,6 +313,7 @@ void Ethash::GPUMiner::workLoop()
 		cnote << "workLoop" << !!m_miner << m_minerSeed << w.seedHash;
 		if (!m_miner || m_minerSeed != w.seedHash)
 		{
+			cnote << "Initialising miner...";
 			m_minerSeed = w.seedHash;
 
 			delete m_miner;
