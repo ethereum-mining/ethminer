4942ae15e11f073442fe91e8aca819dc44e3e40e
subtly <subtly@users.noreply.github.com>
2015-05-21 02:02:57 +0200

    retain session when interacting with capability
    Former-commit-id: 012ffd4adc9cead57b021445cfa3d8267da5b951

--- libp2p/HostCapability.h
+++ libp2p/HostCapability.h
@@ -45,7 +45,7 @@ public:
 
 	Host* host() const { return m_host; }
 
-	std::vector<std::pair<std::shared_ptr<Session>,std::shared_ptr<Peer>>> peerSessions() const;
+	std::vector<std::pair<std::shared_ptr<Session>, std::shared_ptr<Peer>>> peerSessions() const;
 
 protected:
 	virtual std::string name() const = 0;
