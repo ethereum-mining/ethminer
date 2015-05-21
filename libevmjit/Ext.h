87726474ae7498d676d143a1186219193af18e1a
Paweł Bylica <pawel.bylica@imapp.pl>
2015-05-21 17:54:37 +0200

    Cleanup unused code.
    Former-commit-id: 7b5abc152975c7bb1aac908701cf17208e00dc0e

--- libevmjit/Ext.h
+++ libevmjit/Ext.h
@@ -62,7 +62,6 @@ private:
 	Memory& m_memoryMan;
 
 	llvm::Value* m_size;
-	llvm::Value* m_data = nullptr;
 
 	std::array<llvm::Function*, sizeOf<EnvFunc>::value> m_funcs;
 	std::array<llvm::Value*, 8> m_argAllocas;
