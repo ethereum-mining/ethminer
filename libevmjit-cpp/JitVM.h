9a413ec8bbb03391ab9609a940c4a9aa21af5dd7
Paweł Bylica <pawel.bylica@imapp.pl>
2015-05-21 13:11:13 +0200

    Apply recent VM interface changes to EVM JIT.
    Former-commit-id: 5d4fbd4c44aa373082263c2419ebc3065eea39b9

--- libevmjit-cpp/JitVM.h
+++ libevmjit-cpp/JitVM.h
@@ -12,10 +12,14 @@ class JitVM: public VMFace
 {
 	virtual bytesConstRef go(ExtVMFace& _ext, OnOpFunc const& _onOp = {}, uint64_t _steps = (uint64_t)-1) override final;
 
+	virtual u256 gas() const noexcept { return m_gas; }
+	virtual void reset(u256 const& _gas = 0) noexcept { m_gas = _gas; }
+
 private:
 	friend class VMFactory;
-	explicit JitVM(u256 _gas = 0) : VMFace(_gas) {}
+	explicit JitVM(u256 _gas = 0): m_gas(_gas) {}
 
+	u256 m_gas;
 	jit::RuntimeData m_data;
 	jit::ExecutionEngine m_engine;
 	std::unique_ptr<VMFace> m_fallbackVM; ///< VM used in case of input data rejected by JIT
