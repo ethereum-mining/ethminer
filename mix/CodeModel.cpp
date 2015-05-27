bd5bf0b8e9d945ba05a487bde464821f6d8a965c
yann300 <yann.levreau@gmail.com>
2015-05-27 17:26:37 +0200

    Use enum instead of string.

--- mix/CodeModel.cpp
+++ mix/CodeModel.cpp
@@ -380,7 +380,7 @@ void CodeModel::gasEstimation(solidity::CompilerStack const& _cs)
 			GasMeter::GasConsumption cost = gasItem->second;
 			std::stringstream v;
 			v << cost.value;
-			m_gasCostsMaps->push(sourceName, location.start, location.end, QString::fromStdString(v.str()), cost.isInfinite, "statement");
+			m_gasCostsMaps->push(sourceName, location.start, location.end, QString::fromStdString(v.str()), cost.isInfinite, GasMap::type::Statement);
 		}
 
 		if (contractDefinition.getConstructor() != nullptr)
@@ -388,7 +388,7 @@ void CodeModel::gasEstimation(solidity::CompilerStack const& _cs)
 			GasMeter::GasConsumption cost = GasEstimator::functionalEstimation(*_cs.getRuntimeAssemblyItems(n), contractDefinition.getConstructor()->externalSignature());
 			std::stringstream v;
 			v << cost.value;
-			m_gasCostsMaps->push(sourceName, contractDefinition.getConstructor()->getLocation().start, contractDefinition.getConstructor()->getLocation().end, QString::fromStdString(v.str()), cost.isInfinite, "constructor");
+			m_gasCostsMaps->push(sourceName, contractDefinition.getConstructor()->getLocation().start, contractDefinition.getConstructor()->getLocation().end, QString::fromStdString(v.str()), cost.isInfinite, GasMap::type::Constructor);
 		}
 
 		for (auto func: contractDefinition.getDefinedFunctions())
@@ -396,7 +396,7 @@ void CodeModel::gasEstimation(solidity::CompilerStack const& _cs)
 			GasMeter::GasConsumption cost = GasEstimator::functionalEstimation(*_cs.getRuntimeAssemblyItems(n), func->externalSignature());
 			std::stringstream v;
 			v << cost.value;
-			m_gasCostsMaps->push(sourceName, func->getLocation().start, func->getLocation().end, QString::fromStdString(v.str()), cost.isInfinite, "function");
+			m_gasCostsMaps->push(sourceName, func->getLocation().start, func->getLocation().end, QString::fromStdString(v.str()), cost.isInfinite, GasMap::type::Function);
 		}
 	}
 }
@@ -605,9 +605,9 @@ void CodeModel::setOptimizeCode(bool _value)
 	emit scheduleCompilationJob(++m_backgroundJobId);
 }
 
-void GasMapWrapper::push(QString _source, int _start, int _end, QString _value, bool _isInfinite, QString _payload)
+void GasMapWrapper::push(QString _source, int _start, int _end, QString _value, bool _isInfinite, GasMap::type _type)
 {
-	GasMap* gas = new GasMap(_start, _end, _value, _isInfinite, _payload, this);
+	GasMap* gas = new GasMap(_start, _end, _value, _isInfinite, _type, this);
 	m_gasMaps.find(_source).value().push_back(QVariant::fromValue(gas));
 }
 
