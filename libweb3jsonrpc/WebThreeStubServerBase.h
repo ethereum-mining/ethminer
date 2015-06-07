e4aba1fe0bb66ed900523ec57e8d08f378ca9ed3
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-06-07 09:06:06 +0200

    eth_newPendingTransactionFilter
    Former-commit-id: 25002201fb78dd0b1c89a3fd7291ed8783dc4621

--- libweb3jsonrpc/WebThreeStubServerBase.h
+++ libweb3jsonrpc/WebThreeStubServerBase.h
@@ -107,7 +107,8 @@ public:
 	virtual std::string eth_compileSerpent(std::string const& _s);
 	virtual std::string eth_compileSolidity(std::string const& _code);
 	virtual std::string eth_newFilter(Json::Value const& _json);
-	virtual std::string eth_newBlockFilter(std::string const& _filter);
+	virtual std::string eth_newBlockFilter();
+	virtual std::string eth_newPendingTransactionFilter();
 	virtual bool eth_uninstallFilter(std::string const& _filterId);
 	virtual Json::Value eth_getFilterChanges(std::string const& _filterId);
 	virtual Json::Value eth_getFilterLogs(std::string const& _filterId);
