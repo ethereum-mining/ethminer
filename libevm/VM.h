c93457c4387ef02a7967e28c80254a15c818f837
Paweł Bylica <chfast@gmail.com>
2015-06-08 17:23:02 +0200

    VM refactor: check requirements helper function.
    Move an instruction requirements checking in VM to a separated helper function.
    Instruction execution is now split into two parts what reduces OS stack memory usage.
    
    
    Former-commit-id: b58cc2acfd6c7b200c1fb80420c003da9281c1a2

--- libevm/VM.h
+++ libevm/VM.h
@@ -60,10 +60,12 @@ public:
 	u256s const& stack() const { return m_stack; }
 
 private:
+	void checkRequirements(u256& io_gas, ExtVMFace& _ext, OnOpFunc const& _onOp, Instruction _inst);
 	void require(u256 _n, u256 _d) { if (m_stack.size() < _n) { if (m_onFail) m_onFail(); BOOST_THROW_EXCEPTION(StackUnderflow() << RequirementError((bigint)_n, (bigint)m_stack.size())); } if (m_stack.size() - _n + _d > c_stackLimit) { if (m_onFail) m_onFail(); BOOST_THROW_EXCEPTION(OutOfStack() << RequirementError((bigint)(_d - _n), (bigint)m_stack.size())); } }
 	void requireMem(unsigned _n) { if (m_temp.size() < _n) { m_temp.resize(_n); } }
 
 	u256 m_curPC = 0;
+	uint64_t m_steps = 0;
 	bytes m_temp;
 	u256s m_stack;
 	std::vector<uint64_t> m_jumpDests;
