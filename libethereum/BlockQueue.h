15fb3641ceba22d128f9f0a5a5dbf4323a4e1510
Gav Wood <i@gavwood.com>
2015-06-10 23:18:26 +0900

    Avoid throwing out potentially good blocks.
    Former-commit-id: cc668312b77ee3515f30f810b4be092f092710a2

--- libethereum/BlockQueue.h
+++ libethereum/BlockQueue.h
@@ -132,8 +132,8 @@ private:
 
 	mutable Mutex m_verification;										///< Mutex that allows writing to m_verified, m_verifying and m_unverified.
 	std::condition_variable m_moreToVerify;								///< Signaled when m_unverified has a new entry.
-	std::vector<VerifiedBlock> m_verified;				///< List of blocks, in correct order, verified and ready for chain-import.
-	std::deque<VerifiedBlock> m_verifying;				///< List of blocks being verified; as long as the second component (bytes) is empty, it's not finished.
+	std::vector<VerifiedBlock> m_verified;								///< List of blocks, in correct order, verified and ready for chain-import.
+	std::deque<VerifiedBlock> m_verifying;								///< List of blocks being verified; as long as the second component (bytes) is empty, it's not finished.
 	std::deque<std::pair<h256, bytes>> m_unverified;					///< List of blocks, in correct order, ready for verification.
 
 	std::vector<std::thread> m_verifiers;								///< Threads who only verify.
