a652699b3d65e85b5ffac0f8ab17c8b8ae3c5c05
Paweł Bylica <chfast@gmail.com>
2015-06-10 18:01:37 +0200

    Disable some warnings in LLVM headers for GCC/clang compilers.
    Former-commit-id: 9cd4d15e7f69e415c2e626dc924bd4042480110f

--- evmjit/libevmjit/preprocessor/llvm_includes_start.h
+++ evmjit/libevmjit/preprocessor/llvm_includes_start.h
@@ -1,4 +1,12 @@
 #if defined(_MSC_VER)
 	#pragma warning(push)
 	#pragma warning(disable: 4267 4244 4800)
+#elif defined(__clang__)
+	#pragma clang diagnostic push
+	#pragma clang diagnostic ignored "-Wunused-parameter"
+	#pragma clang diagnostic ignored "-Wconversion"
+#else
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wunused-parameter"
+	#pragma GCC diagnostic ignored "-Wconversion"
 #endif
