d9b5f11847337269b29cb5916ba7e100befaa215
arkpar <arkady.paronyan@gmail.com>
2015-06-22 19:13:07 +0200

    improved logging
    Former-commit-id: 017e43336391839b6d3e5c9d8776610d32ed85f7

--- test/libp2p/capability.cpp
+++ test/libp2p/capability.cpp
@@ -42,7 +42,7 @@ struct P2PFixture
 class TestCapability: public Capability
 {
 public:
-	TestCapability(Session* _s, HostCapabilityFace* _h, unsigned _idOffset, CapDesc const&): Capability(_s, _h, _idOffset), m_cntReceivedMessages(0), m_testSum(0) {}
+	TestCapability(std::shared_ptr<Session> _s, HostCapabilityFace* _h, unsigned _idOffset, CapDesc const&): Capability(_s, _h, _idOffset), m_cntReceivedMessages(0), m_testSum(0) {}
 	virtual ~TestCapability() {}
 	int countReceivedMessages() { return m_cntReceivedMessages; }
 	int testSum() { return m_testSum; }
