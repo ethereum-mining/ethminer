69ddff938b613376b5cca06d72c1155441833461
arkpar <arkady.paronyan@gmail.com>
2015-06-22 16:33:35 +0200

    fixing windows build

--- libp2p/Capability.cpp
+++ libp2p/Capability.cpp
@@ -28,7 +28,7 @@ using namespace std;
 using namespace dev;
 using namespace dev::p2p;
 
-Capability::Capability(std::shared_ptr<Session> _s, HostCapabilityFace* _h, unsigned _idOffset): m_session(_s), m_hostCap(_h), m_idOffset(_idOffset)
+Capability::Capability(std::shared_ptr<Session> _s, HostCapabilityFace* _h, unsigned _idOffset): m_session(std::weak_ptr<Session>(_s)), m_hostCap(_h), m_idOffset(_idOffset)
 {
 	clog(NetConnect) << "New session for capability" << m_hostCap->name() << "; idOffset:" << m_idOffset;
 }
