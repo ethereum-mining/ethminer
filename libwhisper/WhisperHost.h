fbe260274597eed13e66593a4b378975d96ea696
Vlad Gluhovsky <gluk256@gmail.com>
2015-06-25 12:40:19 +0200

    Topics of interest advertising made asynchronous
    Former-commit-id: 784a786a82c254339f41146fa051e05a53198da9

--- libwhisper/WhisperHost.h
+++ libwhisper/WhisperHost.h
@@ -51,9 +51,9 @@ public:
 	WhisperHost();
 	virtual ~WhisperHost();
 	unsigned protocolVersion() const { return 2; }
-	void cleanup();
-	std::map<h256, Envelope> all() const { ReadGuard l(x_messages); return m_messages; }
-	FixedHash<TopicBloomFilterSize> const& bloom() const { return m_bloom; }
+	void cleanup(); ///< remove old messages
+	std::map<h256, Envelope> all() const { dev::ReadGuard l(x_messages); return m_messages; }
+	FixedHash<TopicBloomFilterSize> bloom() const { dev::Guard l(m_filterLock); return m_bloom; }
 
 	virtual void inject(Envelope const& _e, WhisperPeer* _from = nullptr) override;
 	virtual Topics const& fullTopics(unsigned _id) const override { try { return m_filters.at(m_watches.at(_id).id).full; } catch (...) { return EmptyTopics; } }
@@ -62,11 +62,12 @@ public:
 	virtual void uninstallWatch(unsigned _watchId) override;
 	virtual h256s peekWatch(unsigned _watchId) const override { dev::Guard l(m_filterLock); try { return m_watches.at(_watchId).changes; } catch (...) { return h256s(); } }
 	virtual h256s checkWatch(unsigned _watchId) override { cleanup(); dev::Guard l(m_filterLock); h256s ret; try { ret = m_watches.at(_watchId).changes; m_watches.at(_watchId).changes.clear(); } catch (...) {} return ret; }
-	virtual h256s watchMessages(unsigned _watchId) override; /// returns IDs of messages, which match specific watch criteria
+	virtual h256s watchMessages(unsigned _watchId) override; ///< returns IDs of messages, which match specific watch criteria
 	virtual Envelope envelope(h256 _m) const override { try { dev::ReadGuard l(x_messages); return m_messages.at(_m); } catch (...) { return Envelope(); } }
 
 protected:
 	virtual void doWork() override;
+	void noteAdvertiseTopicsOfInterest();
 
 private:
 	virtual void onStarting() override { startWorking(); }
@@ -74,7 +75,7 @@ private:
 
 	void streamMessage(h256 _m, RLPStream& _s) const;
 	void noteChanged(h256 _messageHash, h256 _filter);
-	void advertiseTopicsOfInterest();
+	void advertiseTopicsOfInterest(); ///< send our bloom filter to remote peers
 
 	mutable dev::SharedMutex x_messages;
 	std::map<h256, Envelope> m_messages;
