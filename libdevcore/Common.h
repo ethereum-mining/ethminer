d0fa089ce16321dc8b778fd62c9a33896c57b9a6
Vlad Gluhovsky <gluk256@gmail.com>
2015-06-29 18:47:53 +0200

    warning fixed
    Former-commit-id: f70191bb0774a50b1663eeb86f73ab921a4706db

--- libdevcore/Common.h
+++ libdevcore/Common.h
@@ -45,6 +45,7 @@
 #pragma warning(push)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-fpermissive"
 #include <boost/version.hpp>
 #if (BOOST_VERSION == 105800)
 	#include "boost_multiprecision_number_compare_bug_workaround.hpp"
