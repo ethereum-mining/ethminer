a69445be2cdd5b125291042d65595043e71ecd2b
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-07-01 14:06:01 +0200

    isKnown const&
    Former-commit-id: 6811b66994dc949ccb004372bf185cdd29074baa

--- libethereum/ClientBase.h
+++ libethereum/ClientBase.h
@@ -149,9 +149,9 @@ public:
 	/// Get the coinbase address
 	virtual Address address() const override;
 
-	virtual bool isKnown(h256 _hash) const override;
+	virtual bool isKnown(h256 const& _hash) const override;
 	virtual bool isKnown(BlockNumber _block) const override;
-	virtual bool isKnownTransaction(h256 _transactionHash) const override;
+	virtual bool isKnownTransaction(h256 const& _transactionHash) const override;
 
 	/// TODO: consider moving it to a separate interface
 
