91a954dd9da6fc927b9f6b7e455fcfbfd9abbc7e
Paweł Bylica <chfast@gmail.com>
2015-07-03 14:27:17 +0200

    Reapply --vm option changes to eth.

--- eth/main.cpp
+++ eth/main.cpp
@@ -194,7 +194,7 @@ void help()
 		<< "General Options:" << endl
 		<< "    -d,--db-path <path>  Load database from path (default: " << getDataDir() << ")" << endl
 #if ETH_EVMJIT || !ETH_TRUE
-		<< "    -J,--jit  Enable EVM JIT (default: off)." << endl
+		<< "    --vm=<vm-kind>  Select VM. Options are: jit, smart. (default: interpreter)" << endl
 #endif
 		<< "    -v,--verbosity <0 - 9>  Set the log verbosity from 0 to 9 (default: 8)." << endl
 		<< "    -V,--version  Show the version and exit." << endl
@@ -1441,8 +1441,10 @@ int main(int argc, char** argv)
 			}
 		}
 #if ETH_EVMJIT
-		else if (arg == "-J" || arg == "--jit")
-			jit = true;
+		else if (arg == "--vm=jit")
+			VMFactory::setKind(VMKind::JIT);
+		else if (arg == "--vm=smart")
+			VMFactory::setKind(VMKind::Smart);
 #endif
 		else if (arg == "-h" || arg == "--help")
 			help();
@@ -1479,7 +1481,7 @@ int main(int argc, char** argv)
 		g_logPost = [&](std::string const& a, char const*){
 			static SpinLock s_lock;
 			SpinGuard l(s_lock);
-			
+
 			if (g_silence)
 				logbuf += a + "\n";
 			else
@@ -1764,4 +1766,3 @@ int main(int argc, char** argv)
 		writeFile((dbPath.size() ? dbPath : getDataDir()) + "/network.rlp", netData);
 	return 0;
 }
-
