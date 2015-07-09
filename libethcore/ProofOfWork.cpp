1bc77af041c96b2fc7f1734c59a2ea10dff5f3e6
Gav Wood <i@gavwood.com>
2015-07-09 13:03:52 -0700

    Basic Proof-of-Authority sealent.

--- libethcore/ProofOfWork.cpp
+++ libethcore/ProofOfWork.cpp
@@ -20,5 +20,30 @@
  */
 
 #include "ProofOfWork.h"
+#include "BlockInfo.h"
 using namespace std;
 using namespace dev;
+using namespace eth;
+
+const Address BasicAuthority::Authority = Address("1234567890123456789012345678901234567890");
+
+bool BasicAuthority::verify(BlockInfo const& _header)
+{
+	return toAddress(recover(_header.proof.sig, _header.headerHash(WithoutProof))) == Authority;
+}
+
+bool BasicAuthority::preVerify(BlockInfo const& _header)
+{
+	return SignatureStruct(_header.proof.sig).isValid();
+}
+
+BasicAuthority::WorkPackage BasicAuthority::package(BlockInfo const& _header)
+{
+	return WorkPackage{_header.headerHash(WithoutProof)};
+}
+
+void BasicAuthority::Farm::sealBlock(BlockInfo const& _bi)
+{
+	m_onSolutionFound(Solution{sign(m_secret, _bi.headerHash(WithoutProof))});
+}
+
