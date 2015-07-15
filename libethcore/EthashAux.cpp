81430e697cd91d36a7c0119fa9773e6fdf204e97
Gav Wood <i@gavwood.com>
2015-07-16 01:29:03 +0200

    All fields of BlockInfo now private.
    Former-commit-id: 26178d5e92fea2de57d768124688534eb480ded9

--- libethcore/EthashAux.cpp
+++ libethcore/EthashAux.cpp
@@ -63,7 +63,7 @@ EthashAux* EthashAux::get()
 
 uint64_t EthashAux::cacheSize(BlockInfo const& _header)
 {
-	return ethash_get_cachesize((uint64_t)_header.number);
+	return ethash_get_cachesize((uint64_t)_header.number());
 }
 
 uint64_t EthashAux::dataSize(uint64_t _blockNumber)
