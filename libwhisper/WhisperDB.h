d3f10740231a5edcf9db935d0df2b7ebfc3414ef
Vlad Gluhovsky <gluk256@gmail.com>
2015-07-20 18:14:02 +0200

    metainformation removed
    Former-commit-id: 79e319655a56a54ce648243821bef618a63cdd24

--- libwhisper/WhisperDB.h
+++ libwhisper/WhisperDB.h
@@ -50,9 +50,6 @@ class WhisperDB
 	void save(dev::h256 const& _key, Envelope const& _e);
 
 private:
-	void streamMetaInfo(RLPStream& _rlp, Envelope const& _e);
-	void readMetaInfo(RLP const& _rlp, Envelope& _e);
-
 	leveldb::ReadOptions m_readOptions;
 	leveldb::WriteOptions m_writeOptions;
 	std::unique_ptr<leveldb::DB> m_db;
