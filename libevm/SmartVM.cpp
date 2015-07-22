69bbae0072638a88b3d2d722167fba24713e165f
Paweł Bylica <chfast@gmail.com>
2015-07-22 17:03:22 +0200

    concurrent_queue

--- libevm/SmartVM.cpp
+++ libevm/SmartVM.cpp
@@ -21,9 +21,7 @@
 #include "SmartVM.h"
 #include <unordered_map>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <queue>
+#include <libdevcore/concurrent_queue.h>
 #include <libdevcore/Log.h>
 #include <libdevcore/SHA3.h>
 #include <libdevcore/Guards.h>
@@ -33,36 +31,6 @@
 
 namespace dev
 {
-
-template<typename _T, typename _QueueT = std::queue<_T>>
-class concurrent_queue
-{
-public:
-	template<typename _U>
-	void push(_U&& _elem)
-	{
-		{
-			std::lock_guard<decltype(x_mutex)> guard{x_mutex};
-			m_queue.push(std::forward<_U>(_elem));
-		}
-		m_cv.notify_one();
-	}
-
-	_T pop()
-	{
-		std::unique_lock<std::mutex> lock{x_mutex};
-		m_cv.wait(lock, [this]{ return !m_queue.empty(); });
-		auto item = std::move(m_queue.front());
-		m_queue.pop();
-		return item;
-	}
-
-private:
-	_QueueT m_queue;
-	std::mutex x_mutex;
-	std::condition_variable m_cv;
-};
-
 namespace eth
 {
 namespace
