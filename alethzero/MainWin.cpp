ad4deb690dd28778260893f9896b2fed1cb2669f
Gav Wood <i@gavwood.com>
2015-07-25 22:13:49 +0200

    Fix const& where it should have been value.
    Former-commit-id: f9604289410f5a4223472dad8bc82864c023e54f

--- alethzero/MainWin.cpp
+++ alethzero/MainWin.cpp
@@ -360,8 +360,7 @@ void Main::refreshWhisper()
 
 void Main::addNewId(QString _ids)
 {
-	Secret const& id = jsToSecret(_ids.toStdString());
-	KeyPair kp(id);
+	KeyPair kp(jsToSecret(_ids.toStdString()));
 	m_myIdentities.push_back(kp);
 	m_server->setIdentities(keysAsVector(owned()));
 	refreshWhisper();
