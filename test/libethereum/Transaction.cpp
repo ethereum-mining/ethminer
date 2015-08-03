f95c844ccde584e98b8bcff85ae80daaf061297b
Dimitry <dimitry@ethdev.com>
2015-08-03 23:33:52 +0300

    Style

--- test/libethereum/Transaction.cpp
+++ test/libethereum/Transaction.cpp
@@ -43,11 +43,11 @@ BOOST_AUTO_TEST_CASE(TransactionConstructor)
 	{
 		Transaction(fromHex("0xf86d800182521c94095e7baea6a6c7c4c2dfeb977efac326af552d870a8e0358ac39584bc98a7c979f984b031ba048b55bfa915ac795c431978d8a6a992b628d557da5ff759b307d495a36649353a0efffd310ac743f371de3b9f7f9cb56c0b28ad43601b4ab949f53faa07bd2c804"), CheckTransaction::Everything);
 	}
-	catch(OutOfGasIntrinsic)
+	catch (OutOfGasIntrinsic)
 	{
 		wasException = true;
 	}
-	catch(Exception)
+	catch (Exception)
 	{
 		BOOST_ERROR("Exception thrown but expected OutOfGasIntrinsic instead");
 	}
