ef1be85649a33277201c4d0eb20bd14b4ae2f1c2
Gav Wood <i@gavwood.com>
2015-08-06 18:56:26 +0200

    Provide hash rate according to interface in #2737. Closed #2737.

--- libethereum/ClientBase.cpp
+++ libethereum/ClientBase.cpp
@@ -21,7 +21,7 @@
  */
 
 #include "ClientBase.h"
-
+#include <algorithm>
 #include <libdevcore/StructuredLogger.h>
 #include "BlockChain.h"
 #include "Executive.h"
@@ -492,11 +492,10 @@ int ClientBase::compareBlockHashes(h256 _h1, h256 _h2) const
 	BlockNumber n1 = numberFromHash(_h1);
 	BlockNumber n2 = numberFromHash(_h2);
 
-	if (n1 > n2) {
+	if (n1 > n2)
 		return 1;
-	} else if (n1 == n2) {
+	else if (n1 == n2)
 		return 0;
-	}
 	return -1;
 }
 
@@ -523,4 +522,20 @@ bool ClientBase::isKnownTransaction(h256 const& _transactionHash) const
 bool ClientBase::isKnownTransaction(h256 const& _blockHash, unsigned _i) const
 {
 	return isKnown(_blockHash) && bc().transactions().size() > _i;
+}
+
+void ClientBase::submitExternalHashrate(int _rate, h256 const& _id)
+{
+	m_externalRates[_id] = make_pair(_rate, chrono::steady_clock::now());
+}
+
+uint64_t ClientBase::externalHashrate() const
+{
+	uint64_t ret = 0;
+	for (auto i = m_externalRates.begin(); i != m_externalRates.end();)
+		if (chrono::steady_clock::now() - i->second.second > chrono::seconds(5))
+			i = m_externalRates.erase(i);
+		else
+			ret += i++->second.first;
+	return ret;
 }
