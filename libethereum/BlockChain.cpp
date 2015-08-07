10dce0517e22e1d20d5d17d738cf2309329c076f
Gav Wood <i@gavwood.com>
2015-08-08 00:52:02 +0200

    Fixes for determination of gas.

--- libethereum/BlockChain.cpp
+++ libethereum/BlockChain.cpp
@@ -394,7 +394,7 @@ tuple<ImportRoute, bool, unsigned> BlockChain::sync(BlockQueue& _bq, OverlayDB c
 				// Nonce & uncle nonces already verified in verification thread at this point.
 				ImportRoute r;
 				DEV_TIMED_ABOVE("Block import " + toString(block.verified.info.number()), 500)
-					r = import(block.verified, _stateDB, ImportRequirements::Everything & ~ImportRequirements::ValidSeal & ~ImportRequirements::CheckUncles != 0);
+					r = import(block.verified, _stateDB, (ImportRequirements::Everything & ~ImportRequirements::ValidSeal & ~ImportRequirements::CheckUncles) != 0);
 				fresh += r.liveBlocks;
 				dead += r.deadBlocks;
 				goodTransactions.reserve(goodTransactions.size() + r.goodTranactions.size());
