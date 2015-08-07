058c5138a8660f58e06621215e11ce5557d9d6cd
CJentzsch <jentzsch.software@gmail.com>
2015-08-07 16:24:28 +0200

    compile fix, missing include
    Former-commit-id: 55098515b0f204621085aa7ecc496f03a2a7d6ad

--- test/libwhisper/whisperDB.cpp
+++ test/libwhisper/whisperDB.cpp
@@ -24,6 +24,7 @@ along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 #include <libp2p/Host.h>
 #include <libwhisper/WhisperDB.h>
 #include <libwhisper/WhisperHost.h>
+#include <test/TestHelper.h>
 
 using namespace std;
 using namespace dev;
