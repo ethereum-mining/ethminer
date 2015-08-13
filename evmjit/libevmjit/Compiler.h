299fc12a58195012707c5e91017f7e807fd96f12
Paweł Bylica <chfast@gmail.com>
2015-08-13 13:27:10 +0200

    Resolve jumps after code compilation.

--- evmjit/libevmjit/Compiler.h
+++ evmjit/libevmjit/Compiler.h
@@ -31,9 +31,9 @@ private:
 	std::vector<BasicBlock> createBasicBlocks(code_iterator _begin, code_iterator _end, llvm::SwitchInst& _switchInst);
 
 	void compileBasicBlock(BasicBlock& _basicBlock, class RuntimeManager& _runtimeManager, class Arith256& _arith, class Memory& _memory, class Ext& _ext, class GasMeter& _gasMeter,
-			class Stack& _globalStack, llvm::SwitchInst& _jumpTable);
+			class Stack& _globalStack);
 
-	void fillJumpTable();
+	void resolveJumps();
 
 	/// Compiler options
 	Options const& m_options;
