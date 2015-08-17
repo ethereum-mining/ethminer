cce20124a6a903d25916cd83665d9763016c326e
arkpar <arkady.paronyan@gmail.com>
2015-08-17 08:45:01 +0200

    removed vector_ref::randomize
    Former-commit-id: 76db52d5e9bb648107a4ea5db2135d6a6814f9e4

--- libdevcore/vector_ref.h
+++ libdevcore/vector_ref.h
@@ -5,15 +5,9 @@
 #include <type_traits>
 #include <vector>
 #include <string>
-#include <random>
-#include <boost/random/random_device.hpp>
-#include <boost/random/uniform_int_distribution.hpp>
 
 namespace dev
 {
-
-extern unsigned char s_cleanseCounter;
-extern boost::random_device s_vectorRefEngine;
 
 /**
  * A modifiable reference to an existing object or vector in memory.
@@ -71,20 +65,11 @@ public:
 	void copyTo(vector_ref<typename std::remove_const<_T>::type> _t) const { if (overlapsWith(_t)) memmove(_t.data(), m_data, std::min(_t.size(), m_count) * sizeof(_T)); else memcpy(_t.data(), m_data, std::min(_t.size(), m_count) * sizeof(_T)); }
 	/// Copies the contents of this vector_ref to the contents of @a _t, and zeros further trailing elements in @a _t.
 	void populate(vector_ref<typename std::remove_const<_T>::type> _t) const { copyTo(_t); memset(_t.data() + m_count, 0, std::max(_t.size(), m_count) - m_count); }
-	/// Populate with random data.
-	template <class Engine>
-	void randomize(Engine& _eng)
-	{
-		uint8_t* e = (uint8_t*)end();
-		for (uint8_t* i = (uint8_t*)begin(); i != e; ++i)
-			*i = (uint8_t)boost::random::uniform_int_distribution<uint16_t>(0, 255)(_eng);
-	}
-	/// @returns a random valued object.
-	void randomize() { randomize(s_vectorRefEngine); }
 	/// Securely overwrite the memory.
 	/// @note adapted from OpenSSL's implementation.
 	void cleanse()
 	{
+		static unsigned char s_cleanseCounter = 0;
 		uint8_t* p = (uint8_t*)begin();
 		size_t len = (uint8_t*)end() - p;
 		size_t loop = len;
