f09101cb84040b8652fb2d720cd01e73bec6769c
arkpar <arkady.paronyan@gmail.com>
2015-08-18 17:18:11 +0200

    Sort peers by rating and connection time; handle unexpected status packet
    Former-commit-id: a32952924021f3416f9e703eaf80ebd3bdfb142e

--- libethereum/EthereumHost.cpp
+++ libethereum/EthereumHost.cpp
@@ -181,10 +181,19 @@ void EthereumHost::maintainTransactions()
 
 void EthereumHost::foreachPeer(std::function<bool(std::shared_ptr<EthereumPeer>)> const& _f) const
 {
-	for (auto s: peerSessions())
+	//order peers by protocol, rating, connection age
+	auto sessions = peerSessions();
+	auto sessionLess = [](std::pair<std::shared_ptr<Session>, std::shared_ptr<Peer>> const& _left, std::pair<std::shared_ptr<Session>, std::shared_ptr<Peer>> const& _right)
+		{ return _left.first->rating() == _right.first->rating() ? _left.first->connectionTime() < _right.first->connectionTime() : _left.first->rating() > _right.first->rating(); };
+
+	std::sort(sessions.begin(), sessions.end(), sessionLess);
+	for (auto s: sessions)
 		if (!_f(s.first->cap<EthereumPeer>()))
 			return;
-	for (auto s: peerSessions(c_oldProtocolVersion)) //TODO: remove once v61+ is common
+
+	sessions = peerSessions(c_oldProtocolVersion); //TODO: remove once v61+ is common
+	std::sort(sessions.begin(), sessions.end(), sessionLess);
+	for (auto s: sessions)
 		if (!_f(s.first->cap<EthereumPeer>(c_oldProtocolVersion)))
 			return;
 }
