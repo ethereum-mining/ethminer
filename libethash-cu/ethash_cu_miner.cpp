404c79b8170c4b5ff53df9c787fbc67675c0afba
Jan Willem Penterman <janwillem.penterman@ddbgroup.nl>
2015-08-26 16:22:56 +0200

    upstream merge also likely fixes a bug in device selection many cmd line params changed
    Former-commit-id: 61fa78c242cc8aec247cfa3da442ad0d36dd2c04

--- libethash-cu/ethash_cu_miner.cpp
+++ libethash-cu/ethash_cu_miner.cpp
@@ -28,11 +28,14 @@
 #include <assert.h>
 #include <queue>
 #include <random>
+#include <atomic>
+#include <sstream>
 #include <vector>
 #include <chrono>
 #include <thread>
 #include <libethash/util.h>
 #include <libethash/ethash.h>
+#include <libethash/internal.h>
 #include <cuda_runtime.h>
 #include "ethash_cu_miner.h"
 #include "ethash_cu_miner_kernel_globals.h"
@@ -51,8 +54,36 @@
 
 using namespace std;
 
-unsigned const ethash_cu_miner::c_defaultLocalWorkSize = 128;
-unsigned const ethash_cu_miner::c_defaultGlobalWorkSizeMultiplier = 2048; // * CL_DEFAULT_LOCAL_WORK_SIZE
+unsigned const ethash_cu_miner::c_defaultBlockSize = 128;
+unsigned const ethash_cu_miner::c_defaultGridSize = 2048; // * CL_DEFAULT_LOCAL_WORK_SIZE
+unsigned const ethash_cu_miner::c_defaultNumStreams = 2;
+
+#if defined(_WIN32)
+extern "C" __declspec(dllimport) void __stdcall OutputDebugStringA(const char* lpOutputString);
+static std::atomic_flag s_logSpin = ATOMIC_FLAG_INIT;
+#define ETHCU_LOG(_contents) \
+	do \
+			{ \
+		std::stringstream ss; \
+		ss << _contents; \
+						while (s_logSpin.test_and_set(std::memory_order_acquire)) {} \
+		OutputDebugStringA(ss.str().c_str()); \
+		cerr << ss.str() << endl << flush; \
+		s_logSpin.clear(std::memory_order_release); \
+			} while (false)
+#else
+#define ETHCL_LOG(_contents) cout << "[OPENCL]:" << _contents << endl
+#endif
+
+#define CUDA_SAFE_CALL(call)                                          \
+do {                                                                  \
+	cudaError_t err = call;                                           \
+	if (cudaSuccess != err) {                                         \
+		fprintf(stderr, "Cuda error in func '%s' at line %i : %s.\n", \
+		         __FUNCTION__, __LINE__, cudaGetErrorString(err) );   \
+		exit(EXIT_FAILURE);                                           \
+		}                                                                 \
+} while (0)
 
 ethash_cu_miner::search_hook::~search_hook() {}
 
@@ -70,28 +101,19 @@ std::string ethash_cu_miner::platform_info(unsigned _deviceId)
 	if (device_count == 0)
 		return std::string();
 
-	if (cudaRuntimeGetVersion(&runtime_version) == cudaErrorInvalidValue)
-	{
-		cout << cudaGetErrorString(cudaErrorInvalidValue) << endl;
-		return std::string();
-	}
+	CUDA_SAFE_CALL(cudaRuntimeGetVersion(&runtime_version));
 
 	// use selected default device
 	int device_num = std::min<int>((int)_deviceId, device_count - 1);
-
 	cudaDeviceProp device_props;
-	if (cudaGetDeviceProperties(&device_props, device_num) == cudaErrorInvalidDevice)
-	{
-		cout << cudaGetErrorString(cudaErrorInvalidDevice) << endl;
-		return std::string();
-	}
+
+	CUDA_SAFE_CALL(cudaGetDeviceProperties(&device_props, device_num));
 
 	char platform[5];
 	int version_major = runtime_version / 1000;
 	int version_minor = (runtime_version - (version_major * 1000)) / 10;
 	sprintf(platform, "%d.%d", version_major, version_minor);
-
-
+	
 	char compute[5];
 	sprintf(compute, "%d.%d", device_props.major, device_props.minor);
 
@@ -99,28 +121,85 @@ std::string ethash_cu_miner::platform_info(unsigned _deviceId)
 
 }
 
-int ethash_cu_miner::getNumDevices()
+unsigned ethash_cu_miner::getNumDevices()
 {
 	int device_count;
-
-	if (cudaGetDeviceCount(&device_count) == cudaErrorNoDevice)
-	{
-		cout << cudaGetErrorString(cudaErrorNoDevice) << endl;
-		return 0;
-	}
+	CUDA_SAFE_CALL(cudaGetDeviceCount(&device_count));
 	return device_count;
+}
+
+bool ethash_cu_miner::configureGPU(
+	unsigned _blockSize,
+	unsigned _gridSize,
+	unsigned _numStreams,
+	unsigned _extraGPUMemory,
+	bool	 _highcpu,
+	uint64_t _currentBlock
+	)
+{
+	s_blockSize = _blockSize;
+	s_gridSize = _gridSize;
+	s_extraRequiredGPUMem = _extraGPUMemory;
+	s_numStreams = _numStreams;
+	s_highCPU = _highcpu;
+
+	// by default let's only consider the DAG of the first epoch
+	uint64_t dagSize = ethash_get_datasize(_currentBlock);
+	uint64_t requiredSize = dagSize + _extraGPUMemory;
+	for (unsigned int i = 0; i < getNumDevices(); i++)
+	{
+		cudaDeviceProp props;
+		CUDA_SAFE_CALL(cudaGetDeviceProperties(&props, i));
+		if (props.totalGlobalMem >= requiredSize)
+		{
+			ETHCU_LOG(
+				"Found suitable CUDA device [" << props.name
+				<< "] with " << props.totalGlobalMem << " bytes of GPU memory"
+				);
+			return true;
+		}
+
+		ETHCU_LOG(
+			"CUDA device " << props.name
+			<< " has insufficient GPU memory." << props.totalGlobalMem <<
+			" bytes of memory found < " << requiredSize << " bytes of memory required"
+			);
+	}
+	return false;
+}
+
+unsigned ethash_cu_miner::s_extraRequiredGPUMem;
+unsigned ethash_cu_miner::s_blockSize = ethash_cu_miner::c_defaultBlockSize;
+unsigned ethash_cu_miner::s_gridSize = ethash_cu_miner::c_defaultGridSize;
+unsigned ethash_cu_miner::s_numStreams = ethash_cu_miner::c_defaultNumStreams;
+bool ethash_cu_miner::s_highCPU = false;
+
+void ethash_cu_miner::listDevices()
+{
+	string outString = "\nListing CUDA devices.\nFORMAT: [deviceID] deviceName\n";
+	unsigned int i = 0;
+	for (unsigned int i = 0; i < getNumDevices(); i++)
+	{
+		cudaDeviceProp props;
+		CUDA_SAFE_CALL(cudaGetDeviceProperties(&props, i));
+
+		outString += "[" + to_string(i) + "] " + props.name + "\n";
+		outString += "\tCompute version: " + to_string(props.major) + "." + to_string(props.minor) + "\n";
+		outString += "\tcudaDeviceProp::totalGlobalMem: " + to_string(props.totalGlobalMem) + "\n";
+	}
+	ETHCU_LOG(outString);
 }
 
 void ethash_cu_miner::finish()
 {
-	for (unsigned i = 0; i != m_num_buffers; i++) {
+	for (unsigned i = 0; i != s_numStreams; i++) {
 		cudaStreamDestroy(m_streams[i]);
 		m_streams[i] = 0;
 	}
 	cudaDeviceReset();
 }
 
-bool ethash_cu_miner::init(uint8_t const* _dag, uint64_t _dagSize, unsigned num_buffers, unsigned search_batch_size, unsigned workgroup_size, unsigned _deviceId, bool highcpu)
+bool ethash_cu_miner::init(uint8_t const* _dag, uint64_t _dagSize, unsigned _deviceId)
 {
 	
 	int device_count = getNumDevices();
@@ -147,19 +226,12 @@ bool ethash_cu_miner::init(uint8_t const* _dag, uint64_t _dagSize, unsigned num_
 		return false;
 	}
 	cudaDeviceReset();
-	cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeEightByte);
+	cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
+	cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
 
-	m_num_buffers = num_buffers;
-	m_search_batch_size = search_batch_size;
-
-	m_hash_buf	 = new void *[m_num_buffers];
-	m_search_buf = new uint32_t *[m_num_buffers];
-	m_streams    = new cudaStream_t[m_num_buffers];
-
-	// use requested workgroup size, but we require multiple of 8
-	m_workgroup_size = ((workgroup_size + 7) / 8) * 8;
-
-	m_highcpu = highcpu;
+	m_hash_buf	 = new void *[s_numStreams];
+	m_search_buf = new uint32_t *[s_numStreams];
+	m_streams = new cudaStream_t[s_numStreams];
 
 	// patch source code
 	cudaError result;
@@ -179,7 +251,7 @@ bool ethash_cu_miner::init(uint8_t const* _dag, uint64_t _dagSize, unsigned num_
     result = cudaMemcpy(m_dag_ptr, _dag, _dagSize, cudaMemcpyHostToDevice);
 	
 	// create mining buffers
-	for (unsigned i = 0; i != m_num_buffers; ++i)
+	for (unsigned i = 0; i != s_numStreams; ++i)
 	{		
 		result = cudaMallocHost(&m_hash_buf[i], 32 * c_hash_batch_size);
 		result = cudaMallocHost(&m_search_buf[i], (c_max_search_results + 1) * sizeof(uint32_t));
@@ -219,7 +291,7 @@ void ethash_cu_miner::search(uint8_t const* header, uint64_t target, search_hook
 
 	// update header constant buffer
 	cudaMemcpy(m_header, header, 32, cudaMemcpyHostToDevice);
-	for (unsigned i = 0; i != m_num_buffers; ++i)
+	for (unsigned i = 0; i != s_numStreams; ++i)
 	{
 		cudaMemcpy(m_search_buf[i], &c_zero, 4, cudaMemcpyHostToDevice);
 	}
@@ -232,21 +304,21 @@ void ethash_cu_miner::search(uint8_t const* header, uint64_t target, search_hook
 	unsigned buf = 0;
 	std::random_device engine;
 	uint64_t start_nonce = std::uniform_int_distribution<uint64_t>()(engine);
-	for (;; start_nonce += m_search_batch_size)
+	for (;; start_nonce += s_gridSize)
 	{
-		run_ethash_search(m_search_batch_size / m_workgroup_size, m_workgroup_size, m_streams[buf], m_search_buf[buf], m_header, m_dag_ptr, start_nonce, target);	
+		run_ethash_search(s_gridSize, s_blockSize, m_streams[buf], m_search_buf[buf], m_header, m_dag_ptr, start_nonce, target);
 		
 		pending.push({ start_nonce, buf });
-		buf = (buf + 1) % m_num_buffers;
+		buf = (buf + 1) % s_numStreams;
 
 		// read results
-		if (pending.size() == m_num_buffers)
+		if (pending.size() == s_numStreams)
 		{
 			pending_batch const& batch = pending.front();
 
 			uint32_t results[1 + c_max_search_results];
 
-			if (!m_highcpu)
+			if (!s_highCPU)
 				waitStream(m_streams[buf]); // 28ms
 			cudaMemcpyAsync(results, m_search_buf[batch.buf], (1 + c_max_search_results) * sizeof(uint32_t), cudaMemcpyHostToHost, m_streams[batch.buf]);
 
@@ -261,11 +333,11 @@ void ethash_cu_miner::search(uint8_t const* header, uint64_t target, search_hook
 			//	cout << endl;
 			
 			bool exit = num_found && hook.found(nonces, num_found);
-			exit |= hook.searched(batch.start_nonce, m_search_batch_size); // always report searched before exit
+			exit |= hook.searched(batch.start_nonce, s_gridSize * s_blockSize); // always report searched before exit
 			if (exit)
 				break;
 
-			start_nonce += m_search_batch_size;
+			start_nonce += s_gridSize * s_blockSize;
 			// reset search buffer if we're still going
 			if (num_found)
 				cudaMemcpyAsync(m_search_buf[batch.buf], &c_zero, 4, cudaMemcpyHostToDevice, m_streams[batch.buf]);
