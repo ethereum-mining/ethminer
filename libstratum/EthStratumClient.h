b11f3b0e76264f4f791195638717c3d64aa11f2c
Jan Willem Penterman <janwillem.penterman@ddbgroup.nl>
2016-02-09 17:34:37 +0100

    more work in progress
    Former-commit-id: cdee60c2cea25819e000aca15ccd27910d6bb970

---
+++ libstratum/EthStratumClient.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <json/json.h>
+#include <libdevcore/Log.h>
+
+using namespace std;
+using namespace boost::asio;
+using boost::asio::ip::tcp;
+
+class EthStratumClient
+{
+public:
+	EthStratumClient(string const & host, string const & port, string const & user, string const & pass);
+	~EthStratumClient();
+
+	void connect();
+	
+private:
+	void resolve_handler(const boost::system::error_code& ec, tcp::resolver::iterator i);
+	void connect_handler(const boost::system::error_code& ec, tcp::resolver::iterator i);
+	
+	void subscribe_handler(const boost::system::error_code& ec);
+	void authorize_handler(const boost::system::error_code& ec);
+	void read_subscribe_handler(const boost::system::error_code& ec, std::size_t bytes_transferred);
+	void read_authorize_handler(const boost::system::error_code& ec, std::size_t bytes_transferred);
+	void read_work_handler(const boost::system::error_code& ec, std::size_t bytes_transferred);
+
+	string m_host;
+	string m_port;
+	string m_user;
+	string m_pass;
+
+	io_service m_io_service;
+	tcp::socket m_socket;
+	boost::asio::streambuf m_requestBuffer;
+	boost::asio::streambuf m_responseBuffer;
+};
