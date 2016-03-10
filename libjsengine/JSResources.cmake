a2ca5dbdd7b4f40f38f76428b55bdbc2864bf264
Paweł Bylica <chfast@gmail.com>
2016-03-10 22:40:13 +0100

    Remove JS console.
    Former-commit-id: f5e70547e6a02c0ce1d8f0289a36880a512c7796

--- libjsengine/JSResources.cmake
+++
@@ -1,9 +0,0 @@
-
-set(web3 "${CMAKE_CURRENT_LIST_DIR}/../libjsqrc/ethereumjs/dist/web3.js")
-set(admin "${CMAKE_CURRENT_LIST_DIR}/../libjsqrc/admin.js")
-set(pretty_print "${CMAKE_CURRENT_LIST_DIR}/PrettyPrint.js")
-set(common "${CMAKE_CURRENT_LIST_DIR}/Common.js")
-
-set(ETH_RESOURCE_NAME "JSEngineResources")
-set(ETH_RESOURCE_LOCATION "${CMAKE_CURRENT_BINARY_DIR}")
-set(ETH_RESOURCES "web3" "pretty_print" "common" "admin")
