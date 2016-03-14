8f42b02bdb87c447258d616918d24a1e2a180c3b
Paweł Bylica <chfast@gmail.com>
2016-03-14 22:42:11 +0100

    Remove support for RocksDB.
    Former-commit-id: b6101c537f5d1f47ca17c6f3506ce3edb7dc3945

--- libdevcore/db.h
+++ libdevcore/db.h
@@ -23,14 +23,8 @@
 
 #pragma warning(push)
 #pragma warning(disable: 4100 4267)
-#if ETH_ROCKSDB || !ETH_TRUE
-#include <rocksdb/db.h>
-#include <rocksdb/write_batch.h>
-namespace ldb = rocksdb;
-#else
 #include <leveldb/db.h>
 #include <leveldb/write_batch.h>
 namespace ldb = leveldb;
-#endif
 #pragma warning(pop)
 #define DEV_LDB 1
