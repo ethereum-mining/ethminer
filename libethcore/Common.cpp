f2b811e98e9b32b7fa3b86c92bfce75203ec5498
Paweł Bylica <chfast@gmail.com>
2016-03-14 23:37:14 +0100

    Remove FatDB support.
    Former-commit-id: ba4a79ae8733af3ae6ac42053a2b921ae9b34ba6

--- libethcore/Common.cpp
+++ libethcore/Common.cpp
@@ -41,15 +41,9 @@ namespace eth
 {
 
 const unsigned c_protocolVersion = 61;
-#if ETH_FATDB
-const unsigned c_minorProtocolVersion = 3;
-const unsigned c_databaseBaseVersion = 9;
-const unsigned c_databaseVersionModifier = 1;
-#else
 const unsigned c_minorProtocolVersion = 2;
 const unsigned c_databaseBaseVersion = 9;
 const unsigned c_databaseVersionModifier = 0;
-#endif
 
 const unsigned c_databaseVersion = c_databaseBaseVersion + (c_databaseVersionModifier << 8) + (23 << 9);
 
