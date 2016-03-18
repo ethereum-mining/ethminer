dbec9dbc23867d4391c662b014320ce408052841
Genoil <jw@meneer.net>
2016-03-18 11:54:24 +0100

    cleanup of chunks and msperbatch host code
    Former-commit-id: 9b0ac9f9da724d5066c4da29f9760081ff7ff740

--- libethash-cl/ethash_cl_miner.h
+++ libethash-cl/ethash_cl_miner.h
@@ -46,7 +46,6 @@ public:
 		unsigned _platformId,
 		unsigned _localWorkSize,
 		unsigned _globalWorkSize,
-		unsigned _msPerBatch,
 		bool _allowCPU,
 		unsigned _extraGPUMemory,
 		uint64_t _currentBlock
@@ -61,16 +60,11 @@ public:
 	void finish();
 	void search(uint8_t const* _header, uint64_t _target, search_hook& _hook);
 
-	void hash_chunk(uint8_t* _ret, uint8_t const* _header, uint64_t _nonce, unsigned _count);
-	void search_chunk(uint8_t const*_header, uint64_t _target, search_hook& _hook);
-
 	/* -- default values -- */
 	/// Default value of the local work size. Also known as workgroup size.
 	static unsigned const c_defaultLocalWorkSize;
 	/// Default value of the global work size as a multiplier of the local work size
 	static unsigned const c_defaultGlobalWorkSizeMultiplier;
-	/// Default value of the milliseconds per global work size (per batch)
-	static unsigned const c_defaultMSPerBatch;
 
 private:
 
@@ -80,18 +74,11 @@ private:
 	cl::Context m_context;
 	cl::CommandQueue m_queue;
 	cl::Kernel m_searchKernel;
-	unsigned int m_dagChunksCount;
-	std::vector<cl::Buffer> m_dagChunks;
+	cl::Buffer m_dag;
 	cl::Buffer m_header;
 	cl::Buffer m_searchBuffer[c_bufferCount];
 	unsigned m_globalWorkSize;
 	bool m_openclOnePointOne;
-	unsigned m_deviceBits;
-
-	/// The step used in the work size adjustment
-	unsigned int m_stepWorkSizeAdjust;
-	/// The Work Size way of adjustment, > 0 when previously increased, < 0 when previously decreased
-	int m_wayWorkSizeAdjust = 0;
 
 	/// The local work size for the search
 	static unsigned s_workgroupSize;
