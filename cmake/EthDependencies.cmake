0effd6af76a0a5cde5d0faffc8ba39cdaefd3833
Paweł Bylica <chfast@gmail.com>
2017-03-20 18:45:50 +0100

    CMake: drop GUI option

--- cmake/EthDependencies.cmake
+++ cmake/EthDependencies.cmake
@@ -125,47 +125,6 @@ endif()
 find_program(ETH_JSON_RPC_STUB jsonrpcstub)
 message(" - jsonrpcstub location    : ${ETH_JSON_RPC_STUB}")
 
-# do not compile GUI
-if (GUI)
-
-# we need json rpc to build alethzero
-	if (NOT JSON_RPC_CPP_FOUND)
-		message (FATAL_ERROR "JSONRPC is required for GUI client")
-	endif()
-
-# find all of the Qt packages
-# remember to use 'Qt' instead of 'QT', cause unix is case sensitive
-# TODO make headless client optional
-
-	set (ETH_QT_VERSION 5.4)
-
-	find_package (Qt5Core ${ETH_QT_VERSION} REQUIRED)
-	find_package (Qt5Gui ${ETH_QT_VERSION} REQUIRED)
-	find_package (Qt5Quick ${ETH_QT_VERSION} REQUIRED)
-	find_package (Qt5Qml ${ETH_QT_VERSION} REQUIRED)
-	find_package (Qt5Network ${ETH_QT_VERSION} REQUIRED)
-	find_package (Qt5Widgets ${ETH_QT_VERSION} REQUIRED)
-	find_package (Qt5WebEngine ${ETH_QT_VERSION} REQUIRED)
-	find_package (Qt5WebEngineWidgets ${ETH_QT_VERSION} REQUIRED)
-
-	# we need to find path to macdeployqt on mac
-	if (APPLE)
-		set (MACDEPLOYQT_APP ${Qt5Core_DIR}/../../../bin/macdeployqt)
-		message(" - macdeployqt path: ${MACDEPLOYQT_APP}")
-	endif()
-	# we need to find path to windeployqt on windows
-	if (WIN32)
-		set (WINDEPLOYQT_APP ${Qt5Core_DIR}/../../../bin/windeployqt)
-		message(" - windeployqt path: ${WINDEPLOYQT_APP}")
-	endif()
-
-	if (APPLE)
-		find_program(ETH_APP_DMG appdmg)
-		message(" - appdmg location : ${ETH_APP_DMG}")
-	endif()
-
-endif() #GUI
-
 # use multithreaded boost libraries, with -mt suffix
 set(Boost_USE_MULTITHREADED ON)
 
