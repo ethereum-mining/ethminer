5209c82ce95d4086afa21003e5d28cb310c98e6b
Paweł Bylica <chfast@gmail.com>
2017-03-21 10:51:48 +0100

    Remove unnecessary code
    Former-commit-id: d1253396632917d38fd4c2667d17928acd88ae0d

--- libdevcore/Log.cpp
+++ libdevcore/Log.cpp
@@ -43,14 +43,6 @@ mutex x_logOverride;
 /// or equal to the currently output verbosity (g_logVerbosity).
 static map<type_info const*, bool> s_logOverride;
 
-bool dev::isChannelVisible(std::type_info const* _ch, bool _default)
-{
-	Guard l(x_logOverride);
-	if (s_logOverride.count(_ch))
-		return s_logOverride[_ch];
-	return _default;
-}
-
 LogOverrideAux::LogOverrideAux(std::type_info const* _ch, bool _value):
 	m_ch(_ch)
 {
