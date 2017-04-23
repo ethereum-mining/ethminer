778b42604d2946573cd18728f4cf8e33de2caa4f
Paweł Bylica <chfast@gmail.com>
2017-04-23 17:32:49 +0200

    Use boost from hunter
    Former-commit-id: cc442b42f289e12dae0844d93fdeba4f1c3ca1e7

--- libdevcore/Guards.h
+++ libdevcore/Guards.h
@@ -24,12 +24,6 @@
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
-#pragma warning(push)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#include <boost/thread.hpp>
-#pragma warning(pop)
-#pragma GCC diagnostic pop
 
 namespace dev
 {
