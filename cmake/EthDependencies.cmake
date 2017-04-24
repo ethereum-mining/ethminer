ae4b8fc4a987fcee0f11fbf5924614f09d07d878
Paweł Bylica <chfast@gmail.com>
2017-04-24 02:00:33 +0200

    Use jsoncpp from hunter
    Former-commit-id: 98fa4e469ec964c149628db8c97e1877ebc22747

--- cmake/EthDependencies.cmake
+++ cmake/EthDependencies.cmake
@@ -22,13 +22,6 @@ set(ETH_SCRIPTS_DIR ${CMAKE_CURRENT_LIST_DIR}/scripts)
 find_program(CTEST_COMMAND ctest)
 message(STATUS "ctest path: ${CTEST_COMMAND}")
 
-# Dependencies must have a version number, to ensure reproducible build. The version provided here is the one that is in the extdep repository. If you use system libraries, version numbers may be different.
-
-# TODO the Jsoncpp package does not yet check for correct version number
-find_package (Jsoncpp 0.60 REQUIRED)
-message(" - Jsoncpp header: ${JSONCPP_INCLUDE_DIRS}")
-message(" - Jsoncpp lib   : ${JSONCPP_LIBRARIES}")
-
 find_package (json_rpc_cpp 0.4 REQUIRED)
 message (" - json-rpc-cpp header: ${JSON_RPC_CPP_INCLUDE_DIRS}")
 message (" - json-rpc-cpp lib   : ${JSON_RPC_CPP_LIBRARIES}")
@@ -42,8 +35,3 @@ endif()
 # find location of jsonrpcstub
 find_program(ETH_JSON_RPC_STUB jsonrpcstub)
 message(" - jsonrpcstub location    : ${ETH_JSON_RPC_STUB}")
-
-if (APPLE)
-	link_directories(/usr/local/lib)
-	include_directories(/usr/local/include)
-endif()
