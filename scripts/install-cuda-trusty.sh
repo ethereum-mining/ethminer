eae9c1e57ccbf27d996230081639295a206b661b
Paweł Bylica <chfast@gmail.com>
2017-05-10 23:18:12 +0200

    Travis CI: Fix CUDA build option
    Former-commit-id: e09ba1ca34c44c0e3764164e0b64af1c81dea892

--- scripts/install-cuda-trusty.sh
+++ scripts/install-cuda-trusty.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 #
-# Install the core CUDA toolkit for a ubuntu-trusty (14.04) system. Requires the
-# CUDA environment variable to be set to the required version.
+# Install the core CUDA_VER toolkit for a ubuntu-trusty (14.04) system. Requires the
+# CUDA_VER environment variable to be set to the required version.
 #
 # Since this script updates environment variables, to execute correctly you must
 # 'source' this script, rather than executing it in a sub-process.
 #
 # Taken from https://github.com/tmcdonell/travis-scripts.
 
-export CUDA=8.0.61-1
-travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
-travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+export CUDA_VER=8.0.61-1
+travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA_VER}_amd64.deb
+travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA_VER}_amd64.deb
 travis_retry sudo apt-get update -qq
-export CUDA_APT=${CUDA:0:3}
+export CUDA_APT=${CUDA_VER:0:3}
 export CUDA_APT=${CUDA_APT/./-}
 travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT}
 travis_retry sudo apt-get clean
-export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
+export CUDA_HOME=/usr/local/cuda-${CUDA_VER:0:3}
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
 export PATH=${CUDA_HOME}/bin:${PATH}
