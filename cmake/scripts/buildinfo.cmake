50e437d29de33d32e2687f3720b6d48c8a110530
Paweł Bylica <chfast@gmail.com>
2017-05-11 13:22:48 +0200

    Remove EthUtils.cmake

--- cmake/scripts/buildinfo.cmake
+++ cmake/scripts/buildinfo.cmake
@@ -23,5 +23,24 @@ set(OUTFILE "${ETH_DST_DIR}/BuildInfo.h")
 
 configure_file("${INFILE}" "${TMPFILE}")
 
-include("${ETH_SOURCE_DIR}/cmake/EthUtils.cmake")
+
+macro(replace_if_different SOURCE DST)
+	set(extra_macro_args ${ARGN})
+	set(options CREATE)
+	set(one_value_args)
+	set(multi_value_args)
+	cmake_parse_arguments(REPLACE_IF_DIFFERENT "${options}" "${one_value_args}" "${multi_value_args}" "${extra_macro_args}")
+
+	if (REPLACE_IF_DIFFERENT_CREATE AND (NOT (EXISTS "${DST}")))
+		file(WRITE "${DST}" "")
+	endif()
+
+	execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files "${SOURCE}" "${DST}" RESULT_VARIABLE DIFFERENT)
+
+	if (DIFFERENT)
+		execute_process(COMMAND ${CMAKE_COMMAND} -E rename "${SOURCE}" "${DST}")
+	else()
+		execute_process(COMMAND ${CMAKE_COMMAND} -E remove "${SOURCE}")
+	endif()
+endmacro()
 replace_if_different("${TMPFILE}" "${OUTFILE}" CREATE)
