e0a073a1eca9690b208dad2ceb94c434d45954e1
Paweł Bylica <chfast@gmail.com>
2017-05-15 18:05:13 +0200

    Travis CI: Limit CUDA packages to install
    Former-commit-id: 6063df7f800c27f3c94b5202bed10ad4761b0037

--- scripts/install-cuda-trusty.sh
+++ scripts/install-cuda-trusty.sh
@@ -14,7 +14,7 @@ travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA_VER}_amd64.deb
 travis_retry sudo apt-get update -qq
 export CUDA_APT=${CUDA_VER:0:3}
 export CUDA_APT=${CUDA_APT/./-}
-travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT}
+travis_retry sudo apt-get install -qy cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
 travis_retry sudo apt-get clean
 export CUDA_HOME=/usr/local/cuda-${CUDA_VER:0:3}
 export PATH=${CUDA_HOME}/bin:${PATH}
