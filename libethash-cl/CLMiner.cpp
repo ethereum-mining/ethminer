fe0aa1553342bae446c5303072fd07b0bd6545f7
Paweł Bylica <chfast@gmail.com>
2017-07-21 13:45:18 +0200

    Upgrade cl.hpp header
    Former-commit-id: cdcb6932d13c4b86ee17dffafc79652a2b9e1f14

--- libethash-cl/CLMiner.cpp
+++ libethash-cl/CLMiner.cpp
@@ -1,25 +1,7 @@
-/*
-	This file is part of cpp-ethereum.
-
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
-*/
-/** @file EthashGPUMiner.cpp
- * @author Gav Wood <i@gavwood.com>
- * @date 2014
- *
- * Determines the PoW algorithm.
- */
+/// OpenCL miner implementation.
+///
+/// @file
+/// @copyright GNU General Public License
 
 #include "CLMiner.h"
 #include <libethash/internal.h>
@@ -459,7 +441,7 @@ bool CLMiner::init(const h256& seed)
 		addDefinition(code, "COMPUTE", computeCapability);
 
 		// create miner OpenCL program
-		cl::Program::Sources sources{{std::move(code)}};
+		cl::Program::Sources sources{{code.data(), code.size()}};
 		cl::Program program(m_context, sources);
 		try
 		{
