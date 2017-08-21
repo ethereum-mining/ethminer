6ec8c8e94867e5d79600862d07c22ba30b8b97b1
Paweł Bylica <chfast@gmail.com>
2017-08-21 15:31:06 +0200

    Improve hashrate log format
    Former-commit-id: bed359d0bc6ae1aa9c911e59603713e4922b0a14

--- ethminer/MinerAux.h
+++ ethminer/MinerAux.h
@@ -775,12 +775,9 @@ private:
 					auto mp = f.miningProgress();
 					f.resetMiningProgress();
 					if (current)
-					{
-						minelog << "Mining on" << current.header << f.getSolutionStats();
-						minelog << mp;
-					}
+						minelog << mp << f.getSolutionStats();
 					else
-						minelog << "Getting work package...";
+						minelog << "Waiting for work package...";
 
 					auto rate = mp.rate();
 
@@ -919,8 +916,7 @@ private:
 				{
 					if (client.current())
 					{
-						minelog << "Mining on" << client.currentHeaderHash() << f.getSolutionStats();
-						minelog << mp;
+						minelog << mp << f.getSolutionStats();
 					}
 					else
 					{
@@ -959,8 +955,7 @@ private:
 				{
 					if (client.current())
 					{
-						minelog << "Mining on" << client.currentHeaderHash() << f.getSolutionStats();
-						minelog << mp;
+						minelog << mp << f.getSolutionStats();
 					}
 					else if (client.waitState() == MINER_WAIT_STATE_WORK)
 					{
