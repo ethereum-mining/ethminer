5d73f85683f0b8243435443a4f0eb1476a165636
Marius van der Wijden <m.vanderwijden@live.de>
2017-12-05 22:16:55 +0100

    another fix
    Former-commit-id: c710eea3718df7dd946f0a7062900d452bebf3c7

--- libethash-cuda/CUDAMiner.cpp
+++ libethash-cuda/CUDAMiner.cpp
@@ -184,40 +184,17 @@ void CUDAMiner::workLoop()
 				
 				cnote << "set work; seed: " << "#" + w.seed.hex().substr(0, 8) + ", target: " << "#" + w.boundary.hex().substr(0, 12);
 				if (!m_miner || current.seed != w.seed)
-				{/*
-					unsigned device = s_devices[index] > -1 ? s_devices[index] : index;
-					
-					if (s_dagLoadMode == DAG_LOAD_MODE_SEQUENTIAL)
-					{
-						while (s_dagLoadIndex < index) {
-							this_thread::sleep_for(chrono::milliseconds(100)); 
-						}
-					}
-					else if (s_dagLoadMode == DAG_LOAD_MODE_SINGLE)
-					{
-						if (device != s_dagCreateDevice)
-						{
-							// wait until DAG is created on selected device
-							while (s_dagInHostMemory == NULL) {
-								this_thread::sleep_for(chrono::milliseconds(100));
-							}
-						}
-						else
-						{
-							// reset load index
-							s_dagLoadIndex = 0;
-						}
-					}*/
+				{
 					if(!init(w.seed))
 						break;
 				}
+				current = w;
 			}
-			current = w;
-			uint64_t upper64OfBoundary = (uint64_t)(u64)((u256)w.boundary >> 192);
-			uint64_t startN = w.startNonce;
-			if (w.exSizeBits >= 0)
-				startN = w.startNonce | ((uint64_t)index << (64 - 4 - w.exSizeBits)); // this can support up to 16 devices
-			m_miner->search(w.header.data(), upper64OfBoundary, *m_hook, (w.exSizeBits >= 0), startN);
+			uint64_t upper64OfBoundary = (uint64_t)(u64)((u256)current.boundary >> 192);
+			uint64_t startN = current.startNonce;
+			if (current.exSizeBits >= 0)
+				startN = current.startNonce | ((uint64_t)index << (64 - 4 - current.exSizeBits)); // this can support up to 16 devices
+			m_miner->search(current.header.data(), upper64OfBoundary, *m_hook, (current.exSizeBits >= 0), startN);
 		}
 	}
 	catch (std::runtime_error const& _e)
