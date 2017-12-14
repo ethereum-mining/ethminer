b059d94e51332a29bfa4bd395330b97b59a0875d
Jean Cyr <jean.m.cyr>
2017-12-13 20:36:14 -0500

    Stick with existing coding style.

--- libethcore/Miner.cpp
+++ libethcore/Miner.cpp
@@ -6,10 +6,10 @@ using namespace eth;
 
 unsigned dev::eth::Miner::s_dagLoadMode = 0;
 
-unsigned dev::eth::Miner::s_dagLoadIndex(0);
+unsigned dev::eth::Miner::s_dagLoadIndex = 0;
 
 unsigned dev::eth::Miner::s_dagCreateDevice = 0;
 
-uint8_t* dev::eth::Miner::s_dagInHostMemory(NULL);
+uint8_t* dev::eth::Miner::s_dagInHostMemory = NULL;
 
 
