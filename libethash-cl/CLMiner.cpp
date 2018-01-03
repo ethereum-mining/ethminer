54599e74d62c69169c3fd436eed5d2995dbcbe0c
Jean Cyr <jean.m.cyr>
2018-01-03 10:59:28 -0500

    Remove another redundant eval and count GPU failures
    Former-commit-id: 2b12eb0e910b44d6114c6cd4addf783fbf07635a

--- libethash-cl/CLMiner.cpp
+++ libethash-cl/CLMiner.cpp
@@ -104,8 +104,10 @@ void CLMiner::report(uint64_t _nonce, WorkPackage const& _w)
 	Result r = EthashAux::eval(_w.seed, _w.header, _nonce);
 	if (r.value < _w.boundary)
 		farm.submitProof(Solution{_nonce, r.mixHash, _w.header, _w.seed, _w.boundary, false});
-	else
-		cwarn << "Invalid solution";
+	else {
+		farm.failedSolution();
+		cwarn << "FAILURE: GPU gave incorrect result!";
+	}
 }
 
 void CLMiner::kickOff()
