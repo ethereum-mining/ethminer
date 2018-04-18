e939adb1248744704337250aee7545e7871410e0
Paweł Bylica <chfast@gmail.com>
2018-04-18 13:58:59 +0200

    CMake: Move ProjectEthash from Cable

---
+++ cmake/ProjectEthash.cmake
@@ -0,0 +1,28 @@
+include(ExternalProject)
+include(GNUInstallDirs)
+
+set(prefix ${CMAKE_BINARY_DIR}/deps)
+set(ethash_include_dir ${prefix}/${CMAKE_INSTALL_INCLUDEDIR})
+set(ethash_library ${prefix}/${CMAKE_INSTALL_LIBDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}ethash${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+ExternalProject_Add(
+    ethash-project
+    PREFIX ${prefix}
+    GIT_REPOSITORY https://github.com/chfast/ethash
+    GIT_TAG 19f07a486c919613861dfd4a56b3eaece73c8c3a
+    CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+    -DETHASH_BUILD_TESTS=OFF
+    -DETHASH_INSTALL_CMAKE_CONFIG=OFF
+    -DHUNTER_ENABLED=OFF
+)
+
+add_library(ethash::ethash STATIC IMPORTED)
+file(MAKE_DIRECTORY ${ethash_include_dir})
+set_target_properties(
+    ethash::ethash
+    PROPERTIES
+    IMPORTED_LOCATION ${ethash_library}
+    INTERFACE_INCLUDE_DIRECTORIES ${ethash_include_dir}
+)
+add_dependencies(ethash::ethash ethash-project)
