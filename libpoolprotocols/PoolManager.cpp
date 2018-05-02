cb7950ae64dce7916e008f6f1fd1a73167279e60
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2018-05-02 13:21:00 +0200

    Log "wasted" shares due to lack of connection

--- libpoolprotocols/PoolManager.cpp
+++ libpoolprotocols/PoolManager.cpp
@@ -89,14 +89,28 @@ PoolManager::PoolManager(PoolClient * client, Farm &farm, MinerType const & mine
 
 	m_farm.onSolutionFound([&](Solution sol)
 	{
-		m_submit_time = std::chrono::steady_clock::now();
+		// Solution should passthrough only if client is
+		// properly connected. Otherwise we'll have the bad behavior
+		// to log nonce submission but receive no response
 
-		if (sol.stale)
-			cnote << string(EthYellow "Stale nonce 0x") + toHex(sol.nonce);
-		else
-			cnote << string("Nonce 0x") + toHex(sol.nonce);
+		if (p_client->isConnected()) {
 
-		p_client->submitSolution(sol);
+			m_submit_time = std::chrono::steady_clock::now();
+
+			if (sol.stale)
+				cnote << string(EthYellow "Stale nonce 0x") + toHex(sol.nonce);
+			else
+				cnote << string("Nonce 0x") + toHex(sol.nonce);
+
+			p_client->submitSolution(sol);
+
+		}
+		else {
+
+			cnote << string("Nonce 0x") + toHex(sol.nonce) << "wasted. Waiting for connection ...";
+
+		}
+
 		return false;
 	});
 	m_farm.onMinerRestart([&]() {
