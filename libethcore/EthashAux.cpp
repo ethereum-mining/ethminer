0c8478bd35418f7b804b5e181575ba6dc30257e5
Paweł Bylica <chfast@gmail.com>
2018-05-10 15:57:32 +0200

    Update ethash library

--- libethcore/EthashAux.cpp
+++ libethcore/EthashAux.cpp
@@ -25,7 +25,8 @@ using namespace eth;
 Result EthashAux::eval(int epoch, h256 const& _headerHash, uint64_t _nonce) noexcept
 {
     auto headerHash = ethash::hash256_from_bytes(_headerHash.data());
-    auto result = ethash::managed::hash(epoch, headerHash, _nonce);
+    auto& context = ethash::managed::get_epoch_context(epoch);
+    auto result = ethash::hash_light(context, headerHash, _nonce);
     h256 mix{reinterpret_cast<byte*>(result.mix_hash.bytes), h256::ConstructFromPointer};
     h256 final{reinterpret_cast<byte*>(result.final_hash.bytes), h256::ConstructFromPointer};
     return {final, mix};
