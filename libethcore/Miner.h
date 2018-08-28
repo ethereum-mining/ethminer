b5b643793f8cd87d180dafe88da4f3e3db99977f
Jean Cyr <jean.m.cyr@gmail.com>
2018-08-28 09:27:26 -0400

    A few more

--- libethcore/Miner.h
+++ libethcore/Miner.h
@@ -1,12 +1,12 @@
 /*
- This file is part of cpp-ethereum.
+ This file is part of ethminer.
 
  ethminer is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- cpp-ethereum is distributed in the hope that it will be useful,
+ ethminer is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
