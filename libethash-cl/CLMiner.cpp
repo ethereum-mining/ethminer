483d154f100a39208c13cca4914dca4bff167d2c
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2018-09-26 17:42:43 +0200

    Minor cosmetic log output

--- libethash-cl/CLMiner.cpp
+++ libethash-cl/CLMiner.cpp
@@ -405,10 +405,10 @@ void CLMiner::workLoop()
                         auto sol = Solution{
                             nonce, mix, current, false, std::chrono::steady_clock::now(), m_index};
                         if (sol.stale)
-                            cwarn << "Stale solution: " << EthWhite "0x" << toHex(sol.nonce)
+                            cwarn << "Sol: " << EthWhite "0x" << toHex(sol.nonce) << " STALE"
                                   << EthReset;
                         else
-                            cnote << "Solution: " << EthWhite "0x" << toHex(sol.nonce) << EthReset;
+                            cnote << "Sol: " << EthWhite "0x" << toHex(sol.nonce) << EthReset;
                         g_io_service.post(
                             m_io_strand.wrap(boost::bind(&Farm::submitProof, &Farm::f(), sol)));
                     }
@@ -857,4 +857,3 @@ bool CLMiner::init(int epoch)
     }
     return true;
 }
-
