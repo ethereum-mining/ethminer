2946ba87abb5fdc8e6a09d94c31407f821e20146
hackyminer <hackyminer@gmail.com>
2018-12-14 07:57:45 +0900

    activate chfast/ethash progpow

--- libethcore/EthashAux.cpp
+++ libethcore/EthashAux.cpp
@@ -18,6 +18,7 @@
 #include "EthashAux.h"
 
 #include <ethash/ethash.hpp>
+#include <ethash/progpow.hpp>
 
 using namespace dev;
 using namespace eth;
@@ -27,6 +28,16 @@ Result EthashAux::eval(int epoch, h256 const& _headerHash, uint64_t _nonce) noex
     auto headerHash = ethash::hash256_from_bytes(_headerHash.data());
     auto& context = ethash::get_global_epoch_context(epoch);
     auto result = ethash::hash(context, headerHash, _nonce);
+    h256 mix{reinterpret_cast<byte*>(result.mix_hash.bytes), h256::ConstructFromPointer};
+    h256 final{reinterpret_cast<byte*>(result.final_hash.bytes), h256::ConstructFromPointer};
+    return {final, mix};
+}
+
+Result EthashAux::eval(int epoch, int _block_number, h256 const& _headerHash, uint64_t _nonce) noexcept
+{
+    auto headerHash = ethash::hash256_from_bytes(_headerHash.data());
+    auto& context = ethash::get_global_epoch_context(epoch);
+    auto result = progpow::hash(context, _block_number, headerHash, _nonce);
     h256 mix{reinterpret_cast<byte*>(result.mix_hash.bytes), h256::ConstructFromPointer};
     h256 final{reinterpret_cast<byte*>(result.final_hash.bytes), h256::ConstructFromPointer};
     return {final, mix};
