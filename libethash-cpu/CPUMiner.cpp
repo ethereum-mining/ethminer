73f23b279576d063765c7be16c631905e0b1c3e2
Stefan Oberhumer <StefanOberhumer@users.noreply.github.com>
2018-12-24 11:57:36 +0100

    Cpuminer  adjust computing HashRate (#32)
    * cpuminer.* clang-format
    
    * cpuminer: Increment nonce, more accurate hashrate calculation.
    
    * cpuminer: Reorder display and submit solution.
    
    First submit and then display in console.
    
    * Adapt comment, remove debugging print
    
    * cpuminer: Don't update hashrate twice.

--- libethash-cpu/CPUMiner.cpp
+++ libethash-cpu/CPUMiner.cpp
@@ -47,7 +47,7 @@ along with ethminer.  If not, see <http://www.gnu.org/licenses/>.
 /* MACOSX */
 #elif defined(__linux__)
 /* linux */
-#elif defined(_WINDOWS) || defined(_WIN32)
+#elif defined(_WIN32)
 /* windows */
 #else
 #error "Invalid OS configuration"
@@ -167,7 +167,7 @@ bool CPUMiner::initDevice()
 {
     DEV_BUILD_LOG_PROGRAMFLOW(cpulog, "cp-" << m_index << " CPUMiner::initDevice begin");
 
-    cpulog << "Using CPU: " << m_deviceDescriptor.cpCpuNumber << " " << m_deviceDescriptor.cuName
+    cpulog << "Using CPU: " << m_deviceDescriptor.cpCpuNumer << " " << m_deviceDescriptor.cuName
            << " Memory : " << dev::getFormattedMemory((double)m_deviceDescriptor.totalMemory);
 
 #if defined(__APPLE__) || defined(__MACOSX)
@@ -177,23 +177,23 @@ bool CPUMiner::initDevice()
     int err;
 
     CPU_ZERO(&cpuset);
-    CPU_SET(m_deviceDescriptor.cpCpuNumber, &cpuset);
+    CPU_SET(m_deviceDescriptor.cpCpuNumer, &cpuset);
 
     err = sched_setaffinity(0, sizeof(cpuset), &cpuset);
     if (err != 0)
     {
         cwarn << "Error in func " << __FUNCTION__ << " at sched_setaffinity() \"" << strerror(errno)
               << "\"\n";
-        cwarn << "cp-" << m_index << "could not bind thread to cpu" << m_deviceDescriptor.cpCpuNumber
+        cwarn << "cp-" << m_index << "could not bind thread to cpu" << m_deviceDescriptor.cpCpuNumer
               << "\n";
     }
 #else
-    DWORD_PTR dwThreadAffinityMask = 1i64 << m_deviceDescriptor.cpCpuNumber;
+    DWORD_PTR dwThreadAffinityMask = 1i64 << m_deviceDescriptor.cpCpuNumer;
     DWORD_PTR previous_mask;
     previous_mask = SetThreadAffinityMask(GetCurrentThread(), dwThreadAffinityMask);
     if (previous_mask == NULL)
     {
-        cwarn << "cp-" << m_index << "could not bind thread to cpu" << m_deviceDescriptor.cpCpuNumber
+        cwarn << "cp-" << m_index << "could not bind thread to cpu" << m_deviceDescriptor.cpCpuNumer
               << "\n";
         // Handle Errorcode (GetLastError) ??
     }
@@ -203,11 +203,36 @@ bool CPUMiner::initDevice()
 }
 
 
-void CPUMiner::ethash_search(WorkPackage& _w)
+/*
+ * A new epoch was receifed with last work package (called from Miner::initEpoch())
+ *
+ * If we get here it means epoch has changed so it's not necessary
+ * to check again dag sizes. They're changed for sure
+ * We've all related infos in m_epochContext (.dagSize, .dagNumItems, .lightSize, .lightNumItems)
+ */
+bool CPUMiner::initEpoch_internal()
 {
-    const auto& context = ethash::get_global_epoch_context_full(_w.epoch);
-    auto header = ethash::hash256_from_bytes(_w.header.data());
-    auto boundary = ethash::hash256_from_bytes(_w.boundary.data());
+    return true;
+}
+
+
+/*
+   Miner should stop working on the current block
+   This happens if a
+     * new work arrived                       or
+     * miner should stop (eg exit ethminer)   or
+     * miner should pause
+*/
+void CPUMiner::kick_miner()
+{
+    m_new_work.store(true, std::memory_order_relaxed);
+    m_new_work_signal.notify_one();
+}
+
+
+void CPUMiner::search(const dev::eth::WorkPackage& w)
+{
+    constexpr size_t blocksize = 30;
 
     const auto& context = ethash::get_global_epoch_context_full(w.epoch);
     const auto header = ethash::hash256_from_bytes(w.header.data());
@@ -216,65 +241,31 @@ void CPUMiner::ethash_search(WorkPackage& _w)
 
     while (true)
     {
-
-        // Exit next time around if there's new work awaiting
-        bool t = true;
-        if (m_new_work.compare_exchange_strong(t, false) || paused() || shouldStop())
-            break;
-
-        auto r = ethash::search(context, header, boundary, _w.startNonce, m_settings.batchSize);
-        if (r.solution_found)
+        if (m_new_work.load(std::memory_order_relaxed))  // new work arrived ?
         {
-            h256 mix{reinterpret_cast<byte*>(r.mix_hash.bytes), h256::ConstructFromPointer};
-            auto sol = Solution{r.nonce, mix, _w, std::chrono::steady_clock::now(), m_index};
-
-            cpulog << EthWhite << "Job: " << _w.header.abridged()
-                   << " Sol: " << toHex(sol.nonce, HexPrefix::Add) << EthReset;
-
-            Farm::f().submitProof(sol);
+            m_new_work.store(false, std::memory_order_relaxed);
+            break;
         }
 
-        // Update the hash rate
-        updateHashRate(m_settings.batchSize, 1);
-    }
-}
-
-void dev::eth::CPUMiner::progpow_search(WorkPackage& _w) 
-{
-    const auto& context = progpow::get_global_epoch_context_full(_w.epoch);
-    auto header = progpow::hash256_from_bytes(_w.header.data());
-    auto boundary = progpow::hash256_from_bytes(_w.boundary.data());
-
-    while (true)
-    {
-        // Exit next time around if there's new work awaiting
-        bool t = true;
-        if (m_new_work.compare_exchange_strong(t, false) || paused() || shouldStop())
+        if (shouldStop())
             break;
 
-        auto r = progpow::search(context, _w.block, header, boundary, _w.startNonce, m_settings.batchSize);
+
+        auto r = ethash::search(context, header, boundary, nonce, blocksize);
         if (r.solution_found)
         {
             h256 mix{reinterpret_cast<byte*>(r.mix_hash.bytes), h256::ConstructFromPointer};
-            auto sol = Solution{r.nonce, mix, _w, std::chrono::steady_clock::now(), m_index};
+            auto sol = Solution{r.nonce, mix, w, std::chrono::steady_clock::now(), m_index};
 
-            cpulog << EthWhite << "Job: " << _w.header.abridged()
+            cpulog << EthWhite << "Job: " << w.header.abridged()
                    << " Sol: " << toHex(sol.nonce, HexPrefix::Add) << EthReset;
             Farm::f().submitProof(sol);
         }
         nonce += blocksize;
 
         // Update the hash rate
-        updateHashRate(m_settings.batchSize, 1);
+        updateHashRate(blocksize, 1);
     }
-}
-
-void dev::eth::CPUMiner::compileProgPoWKernel(int _block, int _dagelms) 
-{
-    // CPU miner does not have any kernel to compile
-    // Nevertheless the class must override base class
-    (void)_block;
-    (void)_dagelms;
 }
 
 
@@ -285,10 +276,52 @@ void CPUMiner::workLoop()
 {
     DEV_BUILD_LOG_PROGRAMFLOW(cpulog, "cp-" << m_index << " CPUMiner::workLoop() begin");
 
+    WorkPackage current;
+    current.header = h256();
+
     if (!initDevice())
         return;
 
-    minerLoop();
+    while (!shouldStop())
+    {
+        // Wait for work or 3 seconds (whichever the first)
+        const WorkPackage w = work();
+        if (!w)
+        {
+            boost::system_time const timeout =
+                boost::get_system_time() + boost::posix_time::seconds(3);
+            boost::mutex::scoped_lock l(x_work);
+            m_new_work_signal.timed_wait(l, timeout);
+            continue;
+        }
+
+        if (w.algo == "ethash")
+        {
+            // Epoch change ?
+            if (current.epoch != w.epoch)
+            {
+                if (!initEpoch())
+                    break;  // This will simply exit the thread
+
+                // As DAG generation takes a while we need to
+                // ensure we're on latest job, not on the one
+                // which triggered the epoch change
+                current = w;
+                continue;
+            }
+
+            // Persist most recent job.
+            // Job's differences should be handled at higher level
+            current = w;
+
+            // Start searching
+            search(w);
+        }
+        else
+        {
+            throw std::runtime_error("Algo : " + w.algo + " not yet implemented");
+        }
+    }
 
     DEV_BUILD_LOG_PROGRAMFLOW(cpulog, "cp-" << m_index << " CPUMiner::workLoop() end");
 }
@@ -320,7 +353,7 @@ void CPUMiner::enumDevices(std::map<string, DeviceDescriptor>& _DevicesCollectio
         deviceDescriptor.type = DeviceTypeEnum::Cpu;
         deviceDescriptor.totalMemory = getTotalPhysAvailableMemory();
 
-        deviceDescriptor.cpCpuNumber = i;
+        deviceDescriptor.cpCpuNumer = i;
 
         _DevicesCollection[uniqueId] = deviceDescriptor;
     }
