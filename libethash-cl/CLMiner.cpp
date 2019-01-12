2eb5c359a70f17bcff4cde3b0960c3b2a066f73e
Jean M. Cyr <jean.m.cyr@gmail.com>
2019-01-12 17:46:11 -0500

    Fix missed prog regen if epoch and prog switch coincide

--- libethash-cl/CLMiner.cpp
+++ libethash-cl/CLMiner.cpp
@@ -352,7 +352,7 @@ void CLMiner::workLoop()
                     if (!initEpoch(w.block))
                         break;  // This will simply exit the thread
                 }
-                else if (old_period_seed != period_seed)
+                if (old_period_seed != period_seed)
                 {
                     compileKernel(period_seed);
                 }
