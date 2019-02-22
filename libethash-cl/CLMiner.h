7823de9f2bcf4053697173981707d05462826460
seanb <junglecatsw@gmail.com>
2019-02-22 10:50:35 +0000

    amd app

--- libethash-cl/CLMiner.h
+++ libethash-cl/CLMiner.h
@@ -24,7 +24,7 @@
 #define CL_HPP_CL_1_2_DEFAULT_BUILD true
 #define CL_HPP_TARGET_OPENCL_VERSION 120
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
-#include "CL/cl2.hpp"
+#include <CL/cl2.hpp>
 #pragma GCC diagnostic pop
 
 // macOS OpenCL fix:
