91a5a4dc596c789bb6c4c118f5a17da94b8c4f68
Younghwan Jang <Younghwan.Jang@Cerner.com>
2020-01-12 16:04:19 -0600

    Younghwan - Support Macbook Pro 2019

--- libethash-cl/CLMiner.cpp
+++ libethash-cl/CLMiner.cpp
@@ -454,7 +454,7 @@ void CLMiner::enumDevices(std::map<string, DeviceDescriptor>& _DevicesCollection
     {
         std::string platformName = platforms.at(pIdx).getInfo<CL_PLATFORM_NAME>();
         ClPlatformTypeEnum platformType = ClPlatformTypeEnum::Unknown;
-        if (platformName == "AMD Accelerated Parallel Processing")
+        if (platformName == "AMD Accelerated Parallel Processing" || platformName == "Apple")
             platformType = ClPlatformTypeEnum::Amd;
         else if (platformName == "Clover" || platformName == "Intel Gen OCL Driver")
             platformType = ClPlatformTypeEnum::Clover;
