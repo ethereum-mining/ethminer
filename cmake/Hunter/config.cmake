4469585df221368f689556ea02f8a68e17d800de
James Lamb <admin@oranged.to>
2021-05-04 15:47:16 +1000

    Update ethash to 0.6.0 which adds optimisations for x86 cpu's

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -8,3 +8,10 @@ hunter_config(
     Boost
     VERSION 1.75.0
 )
+
+hunter_config(
+    ethash
+    VERSION 0.6.0
+    SHA1 4bfa26b389d1f89a60053de04b2a29feab20f67b
+    URL https://github.com/chfast/ethash/archive/refs/tags/v0.6.0.tar.gz
+)
